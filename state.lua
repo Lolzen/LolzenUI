9ccfc1595605b8a0af548a30a1736264b9950477
Georg Eitler <funer@gmx.at>
2016-09-26 16:57:25 +0200

    state: fix the StateFrame
    also scale it up a litte

--- state.lua
+++ state.lua
@@ -1,20 +1,14 @@
 -- Moving the Frame itself
-local _G = _G
-
 local function onLogin(self)
-	local wf = _G['WatchFrame']
-	wf:ClearAllPoints()
-	wf.ClearAllPoints = function() end
-	wf:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 30, -15)
-	wf.SetPoint = function() end
-	wf:SetHeight(650)
+	local of = ObjectiveTrackerFrame
+	of:ClearAllPoints()
+	of.ClearAllPoints = function() end
+	of:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 30, -75)
+	of.SetPoint = function() end
+	of:SetHeight(650)
+	of:SetScale(1.2)
 end
 
 local f = CreateFrame("Frame")
 f:SetScript("OnEvent", onLogin)
 f:RegisterEvent("PLAYER_LOGIN")
--- Setting it's size
---TBD
-
--- Make it collapsed at login
---TBD
