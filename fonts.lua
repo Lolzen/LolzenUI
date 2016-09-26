9e76864b17e22e45856c4bea0cee0f39ef8dc4f8
Georg Eitler <funer@gmx.at>
2016-09-26 16:59:26 +0200

    fonts: initial commit
    Change the default fonts per Addon, rather per folder
    Also add Fonts

---
+++ fonts.lua
@@ -0,0 +1,6 @@
+-- change the default font to multible of choice
+
+DAMAGE_TEXT_FONT = "Interface\\AddOns\\LolznUI\\fonts\\DroidSansBold.ttf";
+UNIT_NAME_FONT   = "Interface\\AddOns\\LolznUI\\fonts\\DroidSans.ttf";
+NAMEPLATE_FONT   = "Interface\\AddOns\\LolznUI\\fonts\\DridSans.ttf";
+STANDARD_TEXT_FONT = "Interface\\AddOns\\LolznUI\\fonts\\DroidSans.ttf";
