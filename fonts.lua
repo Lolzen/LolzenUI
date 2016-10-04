1f9105a747d35523ac5cea2595097b5e4b991a0c
Georg Eitler <funer@gmx.at>
2016-10-04 23:18:14 +0200

    fonts: fix wrong path

--- fonts.lua
+++ fonts.lua
@@ -1,6 +1,6 @@
 -- change the default font to multible of choice
 
-DAMAGE_TEXT_FONT = "Interface\\AddOns\\LolznUI\\fonts\\DroidSansBold.ttf";
-UNIT_NAME_FONT   = "Interface\\AddOns\\LolznUI\\fonts\\DroidSans.ttf";
-NAMEPLATE_FONT   = "Interface\\AddOns\\LolznUI\\fonts\\DridSans.ttf";
-STANDARD_TEXT_FONT = "Interface\\AddOns\\LolznUI\\fonts\\DroidSans.ttf";
+DAMAGE_TEXT_FONT = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSansBold.ttf";
+UNIT_NAME_FONT   = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf";
+NAMEPLATE_FONT   = "Interface\\AddOns\\LolzenUI\\fonts\\DridSans.ttf";
+STANDARD_TEXT_FONT = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf";
