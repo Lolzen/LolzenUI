6899ca54fd9f57e0862e312073b983fef094cc5d
Georg Eitler <funer@gmx.at>
2016-11-15 17:28:23 +0100

    state: rework and add autohide in combat

--- state.lua
+++ state.lua
@@ -1,14 +1,32 @@
--- Moving the Frame itself
-local function onLogin(self)
+-- Moving the ObjectiveTrackerFrame and hide it in combat
+local eF = CreateFrame("Frame", "eventFrame", UIParent)
+eF:RegisterEvent("PLAYER_ENTERING_WORLD")
+eF:RegisterEvent("PLAYER_REGEN_DISABLED")
+eF:RegisterEvent("PLAYER_REGEN_ENABLED")
+
+function eF.PLAYER_ENTERING_WORLD()
 	local of = ObjectiveTrackerFrame
 	of:ClearAllPoints()
 	of.ClearAllPoints = function() end
-	of:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 30, -75)
+	of:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 30, -30)
 	of.SetPoint = function() end
 	of:SetHeight(650)
-	of:SetScale(1.2)
 end
 
-local f = CreateFrame("Frame")
-f:SetScript("OnEvent", onLogin)
-f:RegisterEvent("PLAYER_LOGIN")
+function eF.PLAYER_REGEN_DISABLED()
+	local of = ObjectiveTrackerFrame
+	of:Hide()
+end
+
+function eF.PLAYER_REGEN_ENABLED()
+	local of = ObjectiveTrackerFrame
+	of:Show()
+end
+
+eF:SetScript("OnEvent", function(self, event, ...)  
+	if(self[event]) then
+		self[event](self, event, ...)
+	else
+		print("LolzenUI - stateframe debug: "..event)
+	end 
+end)
