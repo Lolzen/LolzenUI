2a0e15b21c1bc4e923d98451a1ea6c3717812e29
Georg Eitler <funer@gmx.at>
2017-01-20 14:09:33 +0100

    fonts: fix typo

--- fonts.lua
+++ fonts.lua
@@ -2,5 +2,5 @@
 
 DAMAGE_TEXT_FONT = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSansBold.ttf";
 UNIT_NAME_FONT   = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf";
-NAMEPLATE_FONT   = "Interface\\AddOns\\LolzenUI\\fonts\\DridSans.ttf";
+NAMEPLATE_FONT   = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf";
 STANDARD_TEXT_FONT = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf";
