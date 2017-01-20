fed6c998ca34dd5503e3323d5a612beb20ef23d9
Georg Eitler <funer@gmx.at>
2017-01-20 14:11:34 +0100

    xp: unify functions and mimic HonorWatchBar too when in a BG

--- xp.lua
+++ xp.lua
@@ -3,6 +3,7 @@
 	   by Lolzen
 ####################]]--
 
+-- CHange the rep colors slightly
 FACTION_BAR_COLORS = {
 	[1] = {r = 1, g = 0.2, b = 0.15},	-- Hated
 	[2] = {r = 0.8, g = 0.3, b = 0.22},	-- Hostile		}
@@ -42,58 +43,74 @@ line2:SetTexture(0, 0, 0, 1)
 
 -- fontsrting
 local xptext = xpbar:CreateFontString(nil, "OVERLAY")
-xptext:SetPoint("BOTTOM", xpbar, "TOP", 0, 2)
+xptext:SetPoint("BOTTOM", xpbar, "TOP", 0, -2)
 xptext:SetParent(UIParent)
-xptext:SetFont("Interface\\AddOns\\LolzenUI\\media\\SEMPRG.TTF", 8, "THINOUTLINE")
+xptext:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\DroidSansBold.ttf", 10, "THINOUTLINE")
 xptext:SetTextColor(1,1,1)
 
--- gather our data every time we get xp
-function xpbar:PLAYER_XP_UPDATE()
-	local xp = UnitXP("player")
-	local maxxp = UnitXPMax("player")
-	xpbar:SetMinMaxValues(0, maxxp)
-	xpbar:SetValue(xp)
-	-- colorize the statusbar
-	if GetXPExhaustion() then
-		xpbar:SetStatusBarColor(0.6, 0, 0.6)
-		xptext:SetFormattedText("%.0f%% (%.0f%%)", xp/maxxp*100, GetXPExhaustion()/maxxp*100)
-		--indicator:SetPoint("
-	else
-		xpbar:SetStatusBarColor(46/255, 103/255, 208/255)
-		xptext:SetFormattedText("%.0f%%", xp/maxxp*100)
-		--indicator:SetPoint("
-	end
-	
-	if UnitLevel("player") == MAX_PLAYER_LEVEL then
-		xptext:SetText(nil)
-	end
-end
-xpbar.PLAYER_LEVEL_UP = xpbar.PLAYER_XP_UPDATE
-
-
--- Reputiation data, text & coloring
-function xpbar:UPDATE_FACTION()
-	local name, standing, min, max, value = GetWatchedFactionInfo()
-	max, min = (max-min), (value-min)
-	local baseColor = FACTION_BAR_COLORS[standing]
-	local color = {}
-	if name then
-		for key, value in pairs(baseColor) do 
-			color[key] = math.min(1, value * 1.25)
-		end
+function xpbar:Update()
+	-- Proprity #1: If in BGs show the HonorXP
+	-- Priority #2: Show REP when ticked on
+	-- Priority #3: Show XP
+	if select(2,IsInInstance()) == "pvp" or select(2,IsInInstance()) == "arena" then
+		local prestige = UnitPrestige("player")
+		local level = UnitHonorLevel("player")
+		local current = UnitHonor("player")
+		local max = UnitHonorMax("player")
 		xpbar:SetMinMaxValues(0, max)
-		xpbar:SetValue(min)
-		xpbar:SetStatusBarColor(color.r, color.g, color.b)
-		xptext:SetText("("..name..") "..min.." / "..max)
+		xpbar:SetValue(current)
+		xpbar:SetStatusBarColor(1, 0.4, 0)
+		xptext:SetFormattedText("%s (%.0f%%)", "[P:"..prestige.."L:"..level.."] "..current.."/"..max, current/max*100)
 	else
-		xpbar.PLAYER_XP_UPDATE()
+		-- Reputation
+		local name, standing, min, max, value = GetWatchedFactionInfo()
+		max, min = (max-min), (value-min)
+		local baseColor = FACTION_BAR_COLORS[standing]
+		local color = {}
+		if name then
+			for key, value in pairs(baseColor) do 
+				color[key] = math.min(1, value * 1.25)
+			end
+			xpbar:SetMinMaxValues(0, max)
+			xpbar:SetValue(min)
+			xpbar:SetStatusBarColor(color.r, color.g, color.b)
+			xptext:SetText("("..name..") "..min.." / "..max)
+		else
+			-- XP
+			local xp = UnitXP("player")
+			local maxxp = UnitXPMax("player")
+			xpbar:SetMinMaxValues(0, maxxp)
+			xpbar:SetValue(xp)
+			-- colorize the statusbar
+			if GetXPExhaustion() then
+				xpbar:SetStatusBarColor(0.6, 0, 0.6)
+				xptext:SetFormattedText("%.0f%% (%.0f%%)", xp/maxxp*100, GetXPExhaustion()/maxxp*100)
+			else
+				xpbar:SetStatusBarColor(46/255, 103/255, 208/255)
+				xptext:SetFormattedText("%.0f%%", xp/maxxp*100)
+			end
+	
+			if UnitLevel("player") == MAX_PLAYER_LEVEL then
+				xptext:SetText(nil)
+			end
+		end
 	end
 end
-
-xpbar.PLAYER_ENTERING_WORLD = xpbar.UPDATE_FACTION
+xpbar.PLAYER_ENTERING_WORLD = xpbar.Update
+xpbar.PLAYER_LEVEL_UP = xpbar.Update
+xpbar.PLAYER_XP_UPDATE = xpbar.Update
+xpbar.UPDATE_FACTION = xpbar.Update
+xpbar.HONOR_LEVEL_UPDATE = xpbar.Update
+xpbar.HONOR_XP_UPDATE = xpbar.Update
+xpbar.HONOR_PRESTIGE_UPDATE = xpbar.Update
+xpbar.UPDATE_EXHAUSTION = xpbar.Update
 
 xpbar:RegisterEvent("PLAYER_XP_UPDATE")
 xpbar:RegisterEvent("PLAYER_LEVEL_UP")
 xpbar:RegisterEvent("UPDATE_FACTION")
+xpbar:RegisterEvent("HONOR_XP_UPDATE")
+xpbar:RegisterEvent("HONOR_LEVEL_UPDATE")
+xpbar:RegisterEvent("HONOR_PRESTIGE_UPDATE")
+xpbar:RegisterEvent("UPDATE_EXHAUSTION")
 xpbar:RegisterEvent("PLAYER_ENTERING_WORLD")
 xpbar:SetScript("OnEvent", function(self, event, ...) self[event](self, event, ...) end)
