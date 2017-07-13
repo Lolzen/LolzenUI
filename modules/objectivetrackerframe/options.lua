412e8d8231e0796b6ec3c9543d21c5c33aa04cc4
Georg Eitler <funer@gmx.at>
2017-07-13 22:29:25 +0200

    objectivetrackerframe: initialize functional option panel

--- modules/objectivetrackerframe/options.lua
+++ modules/objectivetrackerframe/options.lua
@@ -13,26 +13,101 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		local about = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 		about:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
-		about:SetText("MOve the ObjectiveTrackerFrame. Auto collapse on entering combat.")
+		about:SetText("Modify behaviour and position of the ObjectiveTrackerFrame.")
 
-		local notice = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-		notice:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -20)
-		notice:SetText("|cff5599ffTO BE DONE|r")
+		local cb1 = CreateFrame("CheckButton", "combatcollapse", ns["objectivetracker"], "ChatConfigCheckButtonTemplate")
+		cb1:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -20)
+		combatcollapseText:SetText("|cff5599ffcombat auto collapse|r")
 
---		local v2 = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
---		v2:SetPoint("TOPLEFT", v1, "BOTTOMLEFT", 0, -8)
---		v2:SetText("|cff5599ffWoW Patch verion:|r "..version)
+		if LolzenUIcfg["objectivetracker_combatcollapse"] == true then
+			cb1:SetChecked(true)
+		else
+			cb1:SetChecked(false)
+		end
 
---		local v3 = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
---		v3:SetPoint("TOPLEFT", v2, "BOTTOMLEFT", 0, 0)
---		v3:SetText("|cff5599ffWoW TOC verion:|r "..tocversion)
+		cb1_desc = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		cb1_desc:SetPoint("TOPLEFT", cb1, "BOTTOMLEFT", 0, 0)
+		cb1_desc:SetText("|cffffffffauto collapse infight and auto expand out of combat|r")
+		
+		local cb2 = CreateFrame("CheckButton", "logincollapse", ns["objectivetracker"], "ChatConfigCheckButtonTemplate")
+		cb2:SetPoint("TOPLEFT", cb1_desc, "BOTTOMLEFT", 0, -8)
+		logincollapseText:SetText("|cff5599ffauto login collapse|r")
 
---		local v4 = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
---		v4:SetPoint("TOPLEFT", v3, "BOTTOMLEFT", 0, 0)
---		v4:SetText("|cff5599ffWoW build:|r "..build)
+		if LolzenUIcfg["objectivetracker_logincollapse"] == true then
+			cb2:SetChecked(true)
+		else
+			cb2:SetChecked(false)
+		end
 
---		local v5 = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
---		v5:SetPoint("TOPLEFT", v4, "BOTTOMLEFT", 0, 0)
---		v4:SetText("|cff5599ffWoW build date: |r"..date)
+		cb2_desc = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		cb2_desc:SetPoint("TOPLEFT", cb2, "BOTTOMLEFT", 0, 0)
+		cb2_desc:SetText("|cffffffffauto collapse on login|r")
+
+		local pos_x_text = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		pos_x_text:SetPoint("TOPLEFT", cb2_desc, "BOTTOMLEFT", 0, -20)
+		pos_x_text:SetText("|cffffffffPosX:")
+
+		local pos_x = CreateFrame("EditBox", nil, ns["objectivetracker"], "InputBoxTemplate")
+		pos_x:SetPoint("LEFT", pos_x_text, "RIGHT", 10, 0)
+		pos_x:SetSize(30, 50)
+		pos_x:SetAutoFocus(false)
+		pos_x:ClearFocus()
+		pos_x:SetNumber(LolzenUIcfg["objectivetracker_posx"])
+		pos_x:SetCursorPosition(0)
+
+		local pos_y_text = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		pos_y_text:SetPoint("LEFT", pos_x, "RIGHT", 5, 0)
+		pos_y_text:SetText("|cffffffffPosX:")
+
+		local pos_y = CreateFrame("EditBox", nil, ns["objectivetracker"], "InputBoxTemplate")
+		pos_y:SetPoint("LEFT", pos_y_text, "RIGHT", 10, 0)
+		pos_y:SetSize(30, 50)
+		pos_y:SetAutoFocus(false)
+		pos_y:ClearFocus()
+		pos_y:SetNumber(LolzenUIcfg["objectivetracker_posy"])
+		pos_y:SetCursorPosition(0)
+
+		local anchor_text = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		anchor_text:SetPoint("LEFT", pos_y, "RIGHT", 5, 0)
+		anchor_text:SetText("|cffffffffAnchor:")
+
+		local anchor_point = CreateFrame("EditBox", nil, ns["objectivetracker"], "InputBoxTemplate")
+		anchor_point:SetPoint("LEFT", anchor_text, "RIGHT", 10, 0)
+		anchor_point:SetSize(100, 50)
+		anchor_point:SetAutoFocus(false)
+		anchor_point:ClearFocus()
+		anchor_point:SetText(LolzenUIcfg["objectivetracker_anchor"])
+		anchor_point:SetCursorPosition(0)
+
+		local pos_desc = ns["objectivetracker"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		pos_desc:SetPoint("TOPLEFT", pos_x_text, "BOTTOMLEFT", 0, -8)
+		pos_desc:SetText("|cffffffffThe startingpoint is the anchor set ("..LolzenUIcfg["objectivetracker_anchor"].." 0/0)")
+
+		ns["objectivetracker"].okay = function(self)
+			if cb1:GetChecked(true) then
+				LolzenUIcfg["objectivetracker_combatcollapse"] = true
+			else
+				LolzenUIcfg["objectivetracker_combatcollapse"] = false
+			end
+			if cb2:GetChecked(true) then
+				LolzenUIcfg["objectivetracker_logincollapse"] = true
+			else
+				LolzenUIcfg["objectivetracker_logincollapse"] = false
+			end
+			LolzenUIcfg["objectivetracker_posx"] = tonumber(pos_x:GetText())
+			LolzenUIcfg["objectivetracker_posy"] = tonumber(pos_y:GetText())
+			LolzenUIcfg["objectivetracker_anchor"] = anchor_point:GetText()
+			LolzenUIcfg["objectivetracker_scale"] = tonumber(anchor_point:GetText())
+		end
+
+		ns["objectivetracker"].default = function(self)
+			LolzenUIcfg["objectivetracker_combatcollapse"] = true
+			LolzenUIcfg["objectivetracker_logincollapse"] = true
+			LolzenUIcfg["objectivetracker_posx"] = 30
+			LolzenUIcfg["objectivetracker_posy"] = -30
+			LolzenUIcfg["objectivetracker_anchor"] = "TOPLEFT"
+			LolzenUIcfg["objectivetracker_scale"] = 1
+			ReloadUI()
+		end
 	end
 end)
