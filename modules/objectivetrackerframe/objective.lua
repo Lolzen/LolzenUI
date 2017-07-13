98b1bf515bc47ca50db97b5340d9d93bd7ffd8e2
Georg Eitler <funer@gmx.at>
2017-07-13 22:28:40 +0200

    objectivetrackerframe: use saved vars

--- modules/objectivetrackerframe/objective.lua
+++ modules/objectivetrackerframe/objective.lua
@@ -9,30 +9,32 @@ f:SetScript("OnEvent", function(self, event, addon)
 		if LolzenUIcfg["objectivetracker"] == false then return end
 
 		local of = ObjectiveTrackerFrame
-		--local hm = ObjectiveTrackerFrame.HeaderMenu
-		--local of_title = of.HeaderMenu.Title
 
 		of:ClearAllPoints()
 		of.ClearAllPoints = function() end
-		of:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 30, -30)
+		of:SetPoint(LolzenUIcfg["objectivetracker_anchor"], UIParent, LolzenUIcfg["objectivetracker_anchor"], LolzenUIcfg["objectivetracker_posx"], LolzenUIcfg["objectivetracker_posy"])
 		of.SetPoint = function() end
 		of:SetHeight(650)
-		ObjectiveTracker_Collapse()
-		of.HeaderMenu.Title:SetPoint("TOPLEFT", UIParent, "TOPLEFT", -140, -35)
+		if LolzenUIcfg["objectivetracker_logincollapse"] == true then
+			ObjectiveTracker_Collapse()
+		end
+		-- bring the HM aesthetically in line with the button, wherever the ObjectiveTrackerFrame's position is set
+		of.HeaderMenu.Title:SetPoint("TOPLEFT", of, "TOPLEFT", -170, -5)
 	end
 end)
 
 local f2 = CreateFrame("Frame")
 f2:RegisterEvent("PLAYER_REGEN_DISABLED")
 f2:RegisterEvent("PLAYER_REGEN_ENABLED")
-
-function f2.PLAYER_REGEN_DISABLED()
-	ObjectiveTracker_Collapse()
-end
-
-function f2.PLAYER_REGEN_ENABLED()
-	local of = ObjectiveTrackerFrame
-	if of.collapsed and not InCombatLockdown() then
-		ObjectiveTracker_Expand()
+f2:SetScript("OnEvent", function(self, event, addon)
+	if event == "PLAYER_REGEN_DISABLED" then
+		if LolzenUIcfg["objectivetracker_combatcollapse"] == false then return end
+		ObjectiveTracker_Collapse()
+	elseif event == "PLAYER_REGEN_ENABLED" then
+		if LolzenUIcfg["objectivetracker_combatcollapse"] == false then return end
+		local of = ObjectiveTrackerFrame
+		if of.collapsed and not InCombatLockdown() then
+			ObjectiveTracker_Expand()
+		end
 	end
-end
+end)
