6b36bd42e66c7d62d5e8e032afa148d143c4e424
Georg Eitler <funer@gmx.at>
2017-07-14 20:43:59 +0200

    fobts: add font options
    and make use of saved vars

--- modules/fonts/fonts.lua
+++ modules/fonts/fonts.lua
@@ -8,9 +8,9 @@ f:SetScript("OnEvent", function(self, event, addon)
 	if addon == "LolzenUI" then
 		if LolzenUIcfg["fonts"] == false then return end
 
-		DAMAGE_TEXT_FONT = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSansBold.ttf";
-		UNIT_NAME_FONT   = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf";
-		NAMEPLATE_FONT   = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf";
-		STANDARD_TEXT_FONT = "Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf";
+		DAMAGE_TEXT_FONT   = "Interface\\AddOns\\LolzenUI\\fonts\\"..LolzenUIcfg["fonts_DAMAGE_TEXT_FONT"]
+		UNIT_NAME_FONT     = "Interface\\AddOns\\LolzenUI\\fonts\\"..LolzenUIcfg["fonts_UNIT_NAME_FONT"]
+		NAMEPLATE_FONT     = "Interface\\AddOns\\LolzenUI\\fonts\\"..LolzenUIcfg["fonts_NAMEPLATE_FONT"]
+		STANDARD_TEXT_FONT = "Interface\\AddOns\\LolzenUI\\fonts\\"..LolzenUIcfg["fonts_STANDARD_TEXT_FONT"]
 	end
 end)
