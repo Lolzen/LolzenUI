85219e3c1c1b695c7ac434dd120943d6a1a2c1de
Georg Eitler <funer@gmx.at>
2017-08-08 16:15:10 +0200

    [WiP] buffs: use saved variables & optimize CPU usage further

--- modules/buffs/buffs.lua
+++ modules/buffs/buffs.lua
@@ -10,103 +10,99 @@ f:SetScript("OnEvent", function(self, event, addon)
 
 		-- Change the position
 		BuffFrame:ClearAllPoints()
-		BuffFrame:SetPoint("TOPRIGHT", Minimap, "TOPLEFT", -15, 2)
+		BuffFrame:SetPoint(LolzenUIcfg["buff_anchor1"], LolzenUIcfg["buff_parent"], LolzenUIcfg["buff_anchor2"], LolzenUIcfg["buff_posx"], LolzenUIcfg["buff_posy"])
 
 		local function StyleBuffs(buttonName, index)
 			local button = _G[buttonName..index]
+			if button and not button.modded then
 
-			-- Debuffborder
-			local border = _G[buttonName..index.."Border"]
-			if border and not border.modded == true then
-				border:SetParent(button)
-				border:SetTexture("Interface\\AddOns\\LolzenUI\\media\\debuffborder")
-				border:SetPoint("TOPRIGHT", button, 0, 0)
-				border:SetPoint("BOTTOMLEFT", button, 0, 0)
-				border:SetTexCoord(0, 1, 0, 1)
-				border.modded = true
-			end
+				-- Size
+				if buttonName == "BuffButton" then
+					button:SetSize(LolzenUIcfg["buff_size"], LolzenUIcfg["buff_size"])
+				elseif buttonName == "DebuffButton" then
+					button:SetSize(LolzenUIcfg["buff_debuff_size"], LolzenUIcfg["buff_debuff_size"])
+				elseif buttonName == "TempEnchant" then
+					button:SetSize(LolzenUIcfg["buff_tempenchant_size"], LolzenUIcfg["buff_tempenchant_size"])
+				end
 
-			-- Auraborder
-			local skin = _G[buttonName..index.."Skin"]
-			if button and not skin and not border and not button.modded == true then
-				local overlay = CreateFrame("Frame", buttonName..index.."Skin", button)
-				overlay:SetAllPoints(button)
-				overlay:SetParent(button)
+				-- Border
+				local border = _G[buttonName..index.."Border"]
+				if border then
+					-- Debuffborder
+					border:SetParent(button)
+					border:SetTexture("Interface\\AddOns\\LolzenUI\\media\\"..LolzenUIcfg["buff_debuff_texture"])
+					border:SetAllPoints(button)
+					border:SetTexCoord(0, 1, 0, 1)
+				else
+					-- Auraborder
+					if not button.border then
+						button.border = button:CreateTexture(nil, "BORDER")
+						button.border:SetTexture("Interface\\AddOns\\LolzenUI\\media\\"..LolzenUIcfg["buff_aura_texture"])
+						button.border:SetAllPoints(button)
+						button.border:SetVertexColor(0, 0, 0)
+					end
+				end
 
-				local texture = overlay:CreateTexture(nil, "BORDER")
-				texture:SetParent(button)
-				texture:SetTexture("Interface\\AddOns\\LolzenUI\\media\\auraborder")
-				texture:SetPoint("TOPRIGHT", overlay, 0, 0)
-				texture:SetPoint("BOTTOMLEFT", overlay, 0, 0)
-				texture:SetVertexColor(0, 0, 0)
+				-- Change the look of the Icon slightly
+				local icon = _G[buttonName..index.."Icon"]
+				if icon then
+					icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+					icon:SetPoint("TOPLEFT", button ,"TOPLEFT", 2, -2)
+					icon:SetPoint("BOTTOMRIGHT", button ,"BOTTOMRIGHT", -2, 2)
+				end
+
+				-- Reposition BuffTimeduration
+				button.duration:ClearAllPoints()
+				button.duration:SetPoint(LolzenUIcfg["buff_duration_anchor1"], button, LolzenUIcfg["buff_duration_anchor2"], LolzenUIcfg["buff_duration_posx"], LolzenUIcfg["buff_duration_posy"])
+				button.duration:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\"..LolzenUIcfg["buff_duration_font"], LolzenUIcfg["buff_duration_font_size"], LolzenUIcfg["buff_duration_font_flag"])
+				button.duration:SetDrawLayer("OVERLAY")
+
+				-- Reposition buffcounters
+				button.count:ClearAllPoints()
+				button.count:SetPoint("TOPRIGHT", button)
+				button.count:SetFont("Fonts\\ARIALN.ttf", 17, "OUTLINE")
+				button.count:SetDrawLayer("OVERLAY")
+				
 				button.modded = true
 			end
-	
-			--Change the look of the Icon slightly
-			local icon = _G[buttonName..index.."Icon"]
-			if icon and not icon.modded == true then
-				icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-				icon:SetPoint("TOPLEFT", button ,"TOPLEFT", 2, -2)
-				icon:SetPoint("BOTTOMRIGHT", button ,"BOTTOMRIGHT", -2, 2)
-				icon.modded = true
-			end
-
-			-- Reposition BuffTimeduration
-			local duration = _G[buttonName..index.."Duration"]
-			if duration and not duration.modded == true then
-				duration:ClearAllPoints()
-				duration:SetPoint("CENTER", button, "BOTTOM", 0, 3)
-				duration:SetFont("Fonts\\ARIALN.ttf", 12, "OUTLINE")
-				duration:SetDrawLayer("OVERLAY")
-				duration.modded = true
-			end
-
-			-- Reposition buffcounters
-			local count = _G[buttonName..index.."Count"]
-			if count and not count.modded == true then
-				count:ClearAllPoints()
-				count:SetPoint("TOPRIGHT", button)
-				count:SetFont("Fonts\\ARIALN.ttf", 17, "OUTLINE")
-				count:SetDrawLayer("OVERLAY")
-				count.modded = true
-			end
 		end
 
-		local function UpdateBuff()
+		local function UpdateAura()
 			for i = 1, BUFF_ACTUAL_DISPLAY do
-				StyleBuffs("BuffButton", i, false)
+				StyleBuffs("BuffButton", i)
 			end
-			for i = 1, 3 do
-				StyleBuffs("TempEnchant", i, false)
+			for i = 1, DEBUFF_MAX_DISPLAY do
+				StyleBuffs("DebuffButton", i)
+			end
+			for i = 1, NUM_TEMP_ENCHANT_FRAMES do
+				StyleBuffs("TempEnchant", i)
 			end
 		end
 
-		local function UpdateDebuff(buttonName, index)
-			StyleBuffs(buttonName, index, true)
-		end
+		hooksecurefunc("BuffFrame_UpdateAllBuffAnchors", UpdateAura)
+		hooksecurefunc("DebuffButton_UpdateAnchors", UpdateAura)
 
-		hooksecurefunc("BuffFrame_UpdateAllBuffAnchors", UpdateBuff)
-		hooksecurefunc("DebuffButton_UpdateAnchors", UpdateDebuff)
-
-		-- Change the timer
-		SecondsToTimeAbbrev = function(time)
-			local hr, m, s, text
-			if time <= 0 then 
-				text = ""
-			elseif(time < 3600 and time > 40) then
-				m = floor(time / 60)
-				s = mod(time, 60)
-				text = (m == 0 and format("|cffffffff%d", s)) or format("|cffffffff%d:%02d", m, s)
-			elseif time < 60 then
-				m = floor(time / 60)
-				s = mod(time, 60)
-				text = (m == 0 and format("|cffffff00%d", s))
-			else
-				hr = floor(time / 3600)
-				m = floor(mod(time, 3600) / 60)
-				text = format("%d:%2d", hr, m)
+		if LolzenUIcfg["buff_duration_detailed"] == true then
+			-- Change the timer
+			SecondsToTimeAbbrev = function(time)
+				local hr, m, s, text
+				if time <= 0 then 
+					text = ""
+				elseif(time < 3600 and time > 40) then
+					m = floor(time / 60)
+					s = mod(time, 60)
+					text = (m == 0 and format("|cffffffff%d", s)) or format("|cffffffff%d:%02d", m, s)
+				elseif time < 60 then
+					m = floor(time / 60)
+					s = mod(time, 60)
+					text = (m == 0 and format("|cffffffff%d", s))
+				else
+					hr = floor(time / 3600)
+					m = floor(mod(time, 3600) / 60)
+					text = format("%d:%2d", hr, m)
+				end
+				return text
 			end
-			return text
 		end
 	end
 end)
