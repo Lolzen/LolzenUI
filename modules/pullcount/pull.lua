76c21b11f35a8083127c9c0ec886f4fefce08b95
Georg Eitler <funer@gmx.at>
2017-08-09 13:44:08 +0200

    pullcount: use options

--- modules/pullcount/pull.lua
+++ modules/pullcount/pull.lua
@@ -9,22 +9,34 @@ f:SetScript("OnEvent", function(self, event, addon)
 		if LolzenUIcfg["pullcount"] == false then return end
 
 		local filter = function(frame, event, message, ...)
-			if message:match("Pull in 3") then
-				PlaySoundFile("Interface\\AddOns\\LolzenUI\\media\\three.mp3", "master")
-			elseif message:match("Pull in 2") then
-				PlaySoundFile("Interface\\AddOns\\LolzenUI\\media\\two.mp3", "master")
-			elseif message:match("Pull in 1") then
-				PlaySoundFile("Interface\\AddOns\\LolzenUI\\media\\one.mp3", "master")
-			elseif message:match(">> Pull Now <<") then
-				PlaySoundFile("Interface\\AddOns\\LolzenUI\\media\\Play.mp3", "master")
+			for i = 1, LolzenUIcfg["pull_count_range"] do
+				local msg = LolzenUIcfg["pull_msg_count"]
+				msg = string.gsub(msg, "!n", i)
+				if message:match(msg) then
+					PlaySoundFile("Interface\\AddOns\\LolzenUI\\sounds\\"..LolzenUIcfg["pull_sound_"..i], "master")
+				end
+			end
+			if message:match(LolzenUIcfg["pull_msg_now"]) then
+				PlaySoundFile("Interface\\AddOns\\LolzenUI\\sounds\\"..LolzenUIcfg["pull_sound_pull"], "master")
 			end
 		end
 
-		ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filter)
-		ChatFrame_AddMessageEventFilter("CHAT_MSG_GUILD", filter)
-		ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY", filter)
-		ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY_LEADER", filter)
-		ChatFrame_AddMessageEventFilter("CHAT_MSG_INSTANCE", filter)
-		ChatFrame_AddMessageEventFilter("CHAT_MSG_INSTANCE_LEADER", filter)
+		if LolzenUIcfg["pull_filter_channel"] == true then
+			ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", filter)
+		end
+		if LolzenUIcfg["pull_filter_guild"] == true then
+			ChatFrame_AddMessageEventFilter("CHAT_MSG_GUILD", filter)
+		end
+		if LolzenUIcfg["pull_filter_party"] == true then
+			ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY", filter)
+			ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY_LEADER", filter)
+		end
+		if LolzenUIcfg["pull_filter_instance"] == true then
+			ChatFrame_AddMessageEventFilter("CHAT_MSG_INSTANCE", filter)
+			ChatFrame_AddMessageEventFilter("CHAT_MSG_INSTANCE_LEADER", filter)
+		end
+		if LolzenUIcfg["pull_filter_say"] == true then
+			ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", filter)
+		end
 	end
 end)
