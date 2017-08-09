4b3d7a35dfb5b4d5438282afa37e5d56cca2ff25
Georg Eitler <funer@gmx.at>
2017-08-09 22:20:23 +0200

    pullcount: remove whitespaces

--- modules/pullcount/options.lua
+++ modules/pullcount/options.lua
@@ -18,11 +18,11 @@ f:SetScript("OnEvent", function(self, event, addon)
 		local about = ns["pullcount"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 		about:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
 		about:SetText("Plays sounds on a specific chat message (pull counter)")
-		
+
 		local pull_message_count_text = ns["pullcount"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 		pull_message_count_text:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -20)
 		pull_message_count_text:SetText("|cff5599ffCountdown message:|r")
-		
+
 		local pull_message_countdown = CreateFrame("EditBox", nil, ns["pullcount"], "InputBoxTemplate")
 		pull_message_countdown:SetPoint("LEFT", pull_message_count_text, "RIGHT", 10, 0)
 		pull_message_countdown:SetSize(100, 20)
@@ -30,15 +30,15 @@ f:SetScript("OnEvent", function(self, event, addon)
 		pull_message_countdown:ClearFocus()
 		pull_message_countdown:SetText(LolzenUIcfg.pullcount["pull_msg_count"])
 		pull_message_countdown:SetCursorPosition(0)
-		
+
 		local count_notice = ns["pullcount"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 		count_notice:SetPoint("TOPLEFT", pull_message_count_text, "BOTTOMLEFT", 0, -8)
 		count_notice:SetText("|cff5599ff!n|r represents a number from 1-10")
-		
+
 		local pull_message_text = ns["pullcount"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 		pull_message_text:SetPoint("TOPLEFT", count_notice, "BOTTOMLEFT", 0, -8)
 		pull_message_text:SetText("|cff5599ffPull now message:|r")
-		
+
 		local pull_message = CreateFrame("EditBox", nil, ns["pullcount"], "InputBoxTemplate")
 		pull_message:SetPoint("LEFT", pull_message_text, "RIGHT", 10, 0)
 		pull_message:SetSize(100, 20)
@@ -46,11 +46,11 @@ f:SetScript("OnEvent", function(self, event, addon)
 		pull_message:ClearFocus()
 		pull_message:SetText(LolzenUIcfg.pullcount["pull_msg_now"])
 		pull_message:SetCursorPosition(0)
-		
+
 		local pull_count_range_text = ns["pullcount"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 		pull_count_range_text:SetPoint("TOPLEFT", pull_message_text, "BOTTOMLEFT", 0, -10)
 		pull_count_range_text:SetText("|cff5599ffPull count range (1-10):|r")
-		
+
 		local pull_count_range = CreateFrame("EditBox", nil, ns["pullcount"], "InputBoxTemplate")
 		pull_count_range:SetPoint("LEFT", pull_count_range_text, "RIGHT", 10, 0)
 		pull_count_range:SetSize(100, 20)
@@ -58,7 +58,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		pull_count_range:ClearFocus()
 		pull_count_range:SetNumber(LolzenUIcfg.pullcount["pull_count_range"])
 		pull_count_range:SetCursorPosition(0)
-		
+
 		local pull_sound_count = {}
 		for i=1, 10 do
 			pull_sound_count[i] = ns["pullcount"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
@@ -68,7 +68,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 				pull_sound_count[i]:SetPoint("TOPLEFT", pull_sound_count[i-1], "BOTTOMLEFT", 0, -10)
 			end
 			pull_sound_count[i]:SetText("|cff5599ffSoundfile for count "..i..":|r Interface\\AddOns\\LolzenUI\\sounds\\")
-			
+
 			pull_sound_count[i].eb = CreateFrame("EditBox", nil, ns["pullcount"], "InputBoxTemplate")
 			pull_sound_count[i].eb:SetPoint("LEFT", pull_sound_count[i], "RIGHT", 10, 0)
 			pull_sound_count[i].eb:SetSize(100, 20)
@@ -77,11 +77,11 @@ f:SetScript("OnEvent", function(self, event, addon)
 			pull_sound_count[i].eb:SetText(LolzenUIcfg.pullcount["pull_sound_"..i])
 			pull_sound_count[i].eb:SetCursorPosition(0)
 		end
-		
+
 		local pull_now_text = ns["pullcount"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
 		pull_now_text:SetPoint("TOPLEFT", pull_sound_count[10], "BOTTOMLEFT", 0, -8)
 		pull_now_text:SetText("|cff5599ffSoundfile for pull now:|r Interface\\AddOns\\LolzenUI\\sounds\\")
-		
+
 		local pull_now = CreateFrame("EditBox", nil, ns["pullcount"], "InputBoxTemplate")
 		pull_now:SetPoint("LEFT", pull_now_text, "RIGHT", 10, 0)
 		pull_now:SetSize(100, 20)
@@ -89,7 +89,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		pull_now:ClearFocus()
 		pull_now:SetText(LolzenUIcfg.pullcount["pull_sound_pull"])
 		pull_now:SetCursorPosition(0)
-		
+
 		local cb1 = CreateFrame("CheckButton", "filter_guild", ns["pullcount"], "ChatConfigCheckButtonTemplate")
 		cb1:SetPoint("TOPLEFT", pull_now_text, "BOTTOMLEFT", 0, -20)
 		filter_guildText:SetText("|cff5599ffplay sounds on countdowns from guild chat|r")
@@ -99,7 +99,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		else
 			cb1:SetChecked(false)
 		end
-		
+
 		local cb2 = CreateFrame("CheckButton", "filter_party", ns["pullcount"], "ChatConfigCheckButtonTemplate")
 		cb2:SetPoint("TOPLEFT", cb1, "BOTTOMLEFT", 0, 0)
 		filter_partyText:SetText("|cff5599ffplay sounds on countdowns from party chat|r")
@@ -109,7 +109,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		else
 			cb2:SetChecked(false)
 		end
-		
+
 		local cb3 = CreateFrame("CheckButton", "filter_instance", ns["pullcount"], "ChatConfigCheckButtonTemplate")
 		cb3:SetPoint("TOPLEFT", cb2, "BOTTOMLEFT", 0, 0)
 		filter_instanceText:SetText("|cff5599ffplay sounds on countdowns from instance chat|r")
@@ -119,7 +119,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		else
 			cb3:SetChecked(false)
 		end
-		
+
 		local cb4 = CreateFrame("CheckButton", "filter_say", ns["pullcount"], "ChatConfigCheckButtonTemplate")
 		cb4:SetPoint("TOPLEFT", cb3, "BOTTOMLEFT", 0, 0)
 		filter_sayText:SetText("|cff5599ffplay sounds on countdowns from say chat|r")
@@ -129,7 +129,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		else
 			cb4:SetChecked(false)
 		end
-		
+
 		local cb5 = CreateFrame("CheckButton", "filter_channel", ns["pullcount"], "ChatConfigCheckButtonTemplate")
 		cb5:SetPoint("TOPLEFT", cb4, "BOTTOMLEFT", 0, 0)
 		filter_channelText:SetText("|cff5599ffplay sounds on countdowns from custom channels|r")
@@ -139,7 +139,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		else
 			cb5:SetChecked(false)
 		end
-		
+
 		ns["pullcount"].okay = function(self)
 			LolzenUIcfg.pullcount["pull_count_range"] = tonumber(pull_count_range:GetText())
 			LolzenUIcfg.pullcount["pull_msg_count"] = pull_message_countdown:GetText()
@@ -181,7 +181,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 				LolzenUIcfg.pullcount["pull_filter_channel"] = false
 			end
 		end
-		
+
 		ns["pullcount"].default = function(self)
 			LolzenUIcfg.pullcount["pull_count_range"] = 3
 			LolzenUIcfg.pullcount["pull_msg_count"] = "Pull in !n"
