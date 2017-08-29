545471b7f6c094b15d8ff65054fe2dfae9ec6363
Georg Eitler <funer@gmx.at>
2017-08-29 20:25:34 +0200

    objectivetrackerframe: use ns.RegisterModule function

--- modules/objectivetrackerframe/options.lua
+++ modules/objectivetrackerframe/options.lua
@@ -2,9 +2,7 @@
 
 local addon, ns = ...
 
-if not ns.modules["actionabars"] then
-	tinsert(ns.modules, "objectivetracker")
-end
+ns.RegisterModule("objectivetracker")
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
