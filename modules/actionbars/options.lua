54fcf230b3865c0f0a8e3819be02fc6c47c29834
Georg Eitler <funer@gmx.at>
2017-08-29 20:20:19 +0200

    actionbars: use ns.RegisterModule function

--- modules/actionbars/options.lua
+++ modules/actionbars/options.lua
@@ -2,9 +2,7 @@
 
 local addon, ns = ...
 
-if not ns.modules["actionabars"] then
-	tinsert(ns.modules, "actionbars")
-end
+ns.RegisterModule("actionbars")
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
