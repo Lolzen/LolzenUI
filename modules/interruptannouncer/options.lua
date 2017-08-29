f3966216eb51a04820f4ff155ecc171546ed5eba
Georg Eitler <funer@gmx.at>
2017-08-29 20:23:49 +0200

    interruptannouncer: use ns.RegisterModule function

--- modules/interruptannouncer/options.lua
+++ modules/interruptannouncer/options.lua
@@ -2,9 +2,7 @@
 
 local addon, ns = ...
 
-if not ns.modules["actionabars"] then
-	tinsert(ns.modules, "interruptannouncer")
-end
+ns.RegisterModule("interruptannouncer")
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
