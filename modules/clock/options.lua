f8feb6566c5193e15f93c64ec122008724011721
Georg Eitler <funer@gmx.at>
2017-08-29 20:22:40 +0200

    clock: use ns.RegisterModule function

--- modules/clock/options.lua
+++ modules/clock/options.lua
@@ -2,9 +2,7 @@
 
 local addon, ns = ...
 
-if not ns.modules["actionabars"] then
-	tinsert(ns.modules, "clock")
-end
+ns.RegisterModule("clock")
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
