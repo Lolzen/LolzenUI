960226597bc21c910079506a3d3b427dc4bf9206
Georg Eitler <funer@gmx.at>
2017-08-29 20:16:28 +0200

    core: add function for modules to register themselves
    modules still have to register themselves, but let the core handle that

--- core.lua
+++ core.lua
@@ -4,3 +4,9 @@ local addon, ns = ...
 
 -- Create an empty table which will be filled by modules
 ns.modules = {}
+
+ns.RegisterModule = function(module)
+	if not ns.modules[module] then
+		tinsert(ns.modules, module)
+	end
+end
