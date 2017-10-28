272074b498bd0ae4e2f18fd9b71a342ad5ab7173
Georg Eitler <funer@gmx.at>
2017-10-28 16:00:59 +0200

    interruptannouncer: use new option functions from core

--- modules/interruptannouncer/options.lua
+++ modules/interruptannouncer/options.lua
@@ -9,14 +9,11 @@ f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, addon)
 	if addon == "LolzenUI" and LolzenUIcfg.modules["interruptannouncer"] == true then
 
-		local title = ns["interruptannouncer"]:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
-		title:SetPoint("TOPLEFT", ns["interruptannouncer"], 16, -16)
-		title:SetText("|cff5599ff"..ns["interruptannouncer"].name.."|r")
+		local title = ns.createTitle("interruptannouncer")
 
-		local about = ns["interruptannouncer"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		local about = ns.createDescription("interruptannouncer", "Announces interrupts")
 		about:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
-		about:SetText("Announces interrupts")
-		
+
 		local cb1 = CreateFrame("CheckButton", "instanceannounce", ns["interruptannouncer"], "ChatConfigCheckButtonTemplate")
 		cb1:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -20)
 		instanceannounceText:SetText("|cff5599ffannounce interrupts in instance chat|r")
@@ -26,7 +23,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		else
 			cb1:SetChecked(false)
 		end
-		
+
 		local cb2 = CreateFrame("CheckButton", "partyannounce", ns["interruptannouncer"], "ChatConfigCheckButtonTemplate")
 		cb2:SetPoint("TOPLEFT", cb1, "BOTTOMLEFT", 0, 0)
 		partyannounceText:SetText("|cff5599ffannounce interrupts in party chat|r")
@@ -36,7 +33,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		else
 			cb2:SetChecked(false)
 		end
-		
+
 		local cb3 = CreateFrame("CheckButton", "sayannounce", ns["interruptannouncer"], "ChatConfigCheckButtonTemplate")
 		cb3:SetPoint("TOPLEFT", cb2, "BOTTOMLEFT", 0, 0)
 		sayannounceText:SetText("|cff5599ffannounce interrupts in say chat|r")
@@ -47,22 +44,15 @@ f:SetScript("OnEvent", function(self, event, addon)
 			cb3:SetChecked(false)
 		end
 
-		local msg_text = ns["interruptannouncer"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
-		msg_text:SetPoint("TOPLEFT", cb3, "BOTTOMLEFT", 0, -8)
-		msg_text:SetText("|cff5599ffMessage:|r")
-		
-		local msg = CreateFrame("EditBox", nil, ns["interruptannouncer"], "InputBoxTemplate")
+		local msg_text = ns.createFonstring("interruptannouncer", "Message:")
+		msg_text:SetPoint("TOPLEFT", cb3, "BOTTOMLEFT", 0, -13)
+
+		local msg = ns.createInputbox("interruptannouncer", 400, 20, LolzenUIcfg.interruptannouncer["interruptannouncer_msg"])
 		msg:SetPoint("LEFT", msg_text, "RIGHT", 10, 0)
-		msg:SetSize(400, 20)
-		msg:SetAutoFocus(false)
-		msg:ClearFocus()
-		msg:SetText(LolzenUIcfg.interruptannouncer["interruptannouncer_msg"])
-		msg:SetCursorPosition(0)
-		
-		local msg_desc = ns["interruptannouncer"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+
+		local msg_desc = ns.createFonstring("interruptannouncer", "use |cff5599ff!spell|r for the interrupted spell and |cff5599ff!name|r for the unit's name which was interrupted")
 		msg_desc:SetPoint("TOPLEFT", msg_text, "BOTTOMLEFT", 0, -8)
-		msg_desc:SetText("use |cff5599ff!spell|r for the interrupted spell and |cff5599ff!name|r for the unit's name which was interrupted")
-		
+
 		ns["interruptannouncer"].okay = function(self)
 			if cb1:GetChecked(true) then
 				LolzenUIcfg.interruptannouncer["interruptannoucer_instance"] = true
