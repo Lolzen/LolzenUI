f251174cb33e0fe322b257f5bda64017966db696
Georg Eitler <funer@gmx.at>
2017-10-28 17:44:16 +0200

    versioncheck: use new option functions from core

--- modules/versioncheck/options.lua
+++ modules/versioncheck/options.lua
@@ -8,35 +8,28 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, addon)
 	if addon == "LolzenUI" and LolzenUIcfg.modules["versioncheck"] == true then
+
 		local version, build, date, tocversion = GetBuildInfo()
 		local addonversion = GetAddOnMetadata(addon, "Version")
 
-		local title = ns["versioncheck"]:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
-		title:SetPoint("TOPLEFT", ns["versioncheck"], 16, -16)
-		title:SetText("|cff5599ff"..ns["versioncheck"].name.."|r")
+		local title = ns.createTitle("versioncheck")
 
-		local about = ns["versioncheck"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		local about = ns.createDescription("versioncheck", "Gives a warning if the verion seems outdated")
 		about:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
-		about:SetText("Gives a warning if the verion seems outdated")
 
-		local v1 = ns["versioncheck"]:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+		local v1 = ns.createHeader("versioncheck", "|cff5599ffAddon verion:|r |cffffffff"..addonversion.."|r")
 		v1:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -20)
-		v1:SetText("|cff5599ffAddon verion:|r |cffffffff"..addonversion.."|r")
 
-		local v2 = ns["versioncheck"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
-		v2:SetPoint("TOPLEFT", v1, "BOTTOMLEFT", 0, -8)
-		v2:SetText("|cff5599ffWoW Patch verion:|r "..version)
+		local v2 = ns.createFonstring("versioncheck", "|cff5599ffWoW Patch verion:|r "..version)
+		v2:SetPoint("TOPLEFT", v1, "BOTTOMLEFT", 0, -15)
 
-		local v3 = ns["versioncheck"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
-		v3:SetPoint("TOPLEFT", v2, "BOTTOMLEFT", 0, 0)
-		v3:SetText("|cff5599ffWoW TOC verion:|r "..tocversion)
+		local v3 = ns.createFonstring("versioncheck", "|cff5599ffWoW TOC verion:|r "..tocversion)
+		v3:SetPoint("TOPLEFT", v2, "BOTTOMLEFT", 0, -8)
 
-		local v4 = ns["versioncheck"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
-		v4:SetPoint("TOPLEFT", v3, "BOTTOMLEFT", 0, 0)
-		v4:SetText("|cff5599ffWoW build:|r "..build)
+		local v4 = ns.createFonstring("versioncheck", "|cff5599ffWoW build:|r "..build)
+		v4:SetPoint("TOPLEFT", v3, "BOTTOMLEFT", 0, -8)
 
-		local v5 = ns["versioncheck"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
-		v5:SetPoint("TOPLEFT", v4, "BOTTOMLEFT", 0, 0)
-		v4:SetText("|cff5599ffWoW build date: |r"..date)
+		local v5 = ns.createFonstring("versioncheck", "|cff5599ffWoW build date: |r"..date)
+		v5:SetPoint("TOPLEFT", v4, "BOTTOMLEFT", 0, -8)
 	end
 end)
