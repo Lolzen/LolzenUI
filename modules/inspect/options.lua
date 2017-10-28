7e696a9d4b8426408940b711efb842458741bf13
Georg Eitler <funer@gmx.at>
2017-10-28 16:14:17 +0200

    inspect: use new option functions from core

--- modules/inspect/options.lua
+++ modules/inspect/options.lua
@@ -8,14 +8,12 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, addon)
 	if addon == "LolzenUI" and LolzenUIcfg.modules["inspect"] == true then
-		local title = ns["inspect"]:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
-		title:SetPoint("TOPLEFT", ns["inspect"], 16, -16)
-		title:SetText("|cff5599ff"..ns["inspect"].name.."|r")
 
-		local about = ns["inspect"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		local title = ns.createTitle("inspect")
+
+		local about = ns.createDescription("inspect", "Fixes unit inspection on Mouseover targets (with keybind)")
 		about:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
-		about:SetText("Fixes unit inspection on Mouseover targets (with keybind)")
-		
+
 		local bignotice = ns["inspect"]:CreateFontString(nil, "ARTWORK")
 		bignotice:SetPoint("CENTER", ns["inspect"], "CENTER", 0, -8)
 		bignotice:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\DroidSansBold.ttf", 28, "OUTLINE")
