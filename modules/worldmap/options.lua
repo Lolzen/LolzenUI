2987b0402ab39a6a52fc0a5e830facc776604e41
Georg Eitler <funer@gmx.at>
2017-10-28 17:50:43 +0200

    worldmap: use new option functions from core

--- modules/worldmap/options.lua
+++ modules/worldmap/options.lua
@@ -9,25 +9,19 @@ f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, addon)
 	if addon == "LolzenUI" and LolzenUIcfg.modules["worldmap"] == true then
 
-		local title = ns["worldmap"]:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
-		title:SetPoint("TOPLEFT", ns["worldmap"], 16, -16)
-		title:SetText("|cff5599ff"..ns["worldmap"].name.."|r")
+		local title = ns.createTitle("worldmap")
 
-		local about = ns["worldmap"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
+		local about = ns.createDescription("worldmap", "Scales the Worldmap")
 		about:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
-		about:SetText("Scales the Worldmap")
 
-		local scale = ns["worldmap"]:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-		scale:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -20)
-		scale:SetText("|cff5599ffSet the WorldMap scale:|r")
+		local scale_text = ns.createFonstring("worldmap", "Worldmap Scale:")
+		scale_text:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -20)
 		
-		local eb = CreateFrame("EditBox", nil, ns["worldmap"], "InputBoxTemplate")
-		eb:SetPoint("LEFT", scale, "RIGHT", 10, 0)
-		eb:SetSize(30, 50)
-		eb:SetAutoFocus(false)
-		eb:ClearFocus()
-		eb:SetNumber(LolzenUIcfg.worldmap["worldmap_scale"])
-		eb:SetCursorPosition(0)
+		local desc = ns.createFonstring("worldmap", "Only use numbers or numbers with decimals (0.75), otherwise it will default to 1")
+		desc:SetPoint("TOPLEFT", scale_text, "BOTTOMLEFT", 0, -8)
+
+		local scale = ns.createInputbox("worldmap", 30, 20, LolzenUIcfg.worldmap["worldmap_scale"])
+		scale:SetPoint("LEFT", scale_text, "RIGHT", 10, 0)
 
 		ns["worldmap"].okay = function(self)
 			LolzenUIcfg.worldmap["worldmap_scale"] = tonumber(eb:GetText())
@@ -37,9 +31,5 @@ f:SetScript("OnEvent", function(self, event, addon)
 			LolzenUIcfg.worldmap["worldmap_scale"] = 1
 			ReloadUI()
 		end
-
-		local desc = ns["worldmap"]:CreateFontString(nil, "ARTWORK", "GameFontHighlightSmall")
-		desc:SetPoint("TOPLEFT", scale, "BOTTOMLEFT", 0, -16)
-		desc:SetText("|cffffffffOnly use numbers or numbers with decimals (0.75), otherwise it will default to 1|r")
 	end
 end)
