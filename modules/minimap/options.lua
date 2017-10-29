e8863ed1ce27b905ae38d09465f6571f8740fa99
Georg Eitler <funer@gmx.at>
2017-10-29 17:57:31 +0100

    minimap: use createCheckBox function from core

--- modules/minimap/options.lua
+++ modules/minimap/options.lua
@@ -14,15 +14,8 @@ f:SetScript("OnEvent", function(self, event, addon)
 		local about = ns.createDescription("minimap", "A clean Minimap")
 		about:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -8)
 
-		local cb1 = CreateFrame("CheckButton", "squareMinimap", ns["minimap"], "ChatConfigCheckButtonTemplate")
+		local cb1 = ns.createCheckBox("minimap", "squareMinimap", "|cff5599ffsquare Minimap|r", LolzenUIcfg.minimap["minimap_square"])
 		cb1:SetPoint("TOPLEFT", about, "BOTTOMLEFT", 0, -20)
-		squareMinimapText:SetText("|cff5599ffsquare Minimap|r")
-
-		if LolzenUIcfg.minimap["minimap_square"] == true then
-			cb1:SetChecked(true)
-		else
-			cb1:SetChecked(false)
-		end
 
 		ns["minimap"].okay = function(self)
 			if cb1:GetChecked(true) then
