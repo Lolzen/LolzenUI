e7b93d4260a42b251ecf423b010a63d8975f4df3
Georg Eitler <funer@gmx.at>
2017-11-06 03:27:00 +0100

    nameplates: [WiP] update to use saved vars

--- modules/nameplates/plates.lua
+++ modules/nameplates/plates.lua
@@ -15,7 +15,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 			nameplateLargerScale = 1,
 			nameplateMaxScale = 1,
 			nameplateMinScale = 1,
-			nameplateSelectedScale = 1.2,
+			nameplateSelectedScale = LolzenUIcfg.nameplates["np_selected_scale"],
 			nameplateSelfScale = 1,
 		}
 
@@ -53,7 +53,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 				return ("|cff%02x%02x%02x%d|r"):format(GetQuestDifficultyColor(UnitLevel(unit)).r*255, GetQuestDifficultyColor(UnitLevel(unit)).g*255, GetQuestDifficultyColor(UnitLevel(unit)).b*255, UnitLevel(unit))
 			end
 		end
-		
+
 		tags["lolzen:npname"] = function(unit)
 			return UnitName(unit)
 		end
@@ -63,7 +63,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 				-- health bar
 				local health = CreateFrame("StatusBar", nil, frame)
 				health:SetAllPoints()
-				health:SetStatusBarTexture("Interface\\AddOns\\LolzenUI\\media\\statusbar")
+				health:SetStatusBarTexture("Interface\\AddOns\\LolzenUI\\media\\"..LolzenUIcfg.nameplates["np_texture"])
 				health.colorHealth = true
 				health.colorTapping = true
 				health.colorDisconnected = true
@@ -71,90 +71,94 @@ f:SetScript("OnEvent", function(self, event, addon)
 				health.colorClass = true
 				health.colorReaction = true
 				frame.Health = health
- 
+
 				-- frame background
 				local bg = frame:CreateTexture(nil, "BACKGROUND")
 				bg:SetAllPoints()
 				bg:SetColorTexture(0, 0, 0, 0.5)
 
 				local levelname = health:CreateFontString(nil, "OVERLAY")
-				levelname:SetPoint("CENTER", health, 0, 3) 
-				levelname:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf", 6, "THINOUTLINE")
+				levelname:SetPoint(LolzenUIcfg.nameplates["np_lvlname_anchor"], health, LolzenUIcfg.nameplates["np_lvlname_posx"], LolzenUIcfg.nameplates["np_lvlname_posy"]) 
+				levelname:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\"..LolzenUIcfg.nameplates["np_lvlname_font"], LolzenUIcfg.nameplates["np_lvlname_font_size"], LolzenUIcfg.nameplates["np_lvlname_font_flag"])
 				frame.Level = levelname
 				frame:Tag(levelname, '[lolzen:nplevel][shortclassification] [lolzen:npname]')
 
 				local Castbar = CreateFrame("StatusBar", nil, health)
-				Castbar:SetStatusBarTexture("Interface\\AddOns\\LolzenUI\\media\\statusbar")
-				Castbar:SetPoint("TOP", health, "BOTTOM", 0, 1)
-				Castbar:SetSize(100, 1)			
+				Castbar:SetStatusBarTexture("Interface\\AddOns\\LolzenUI\\media\\"..LolzenUIcfg.nameplates["np_cb_texture"])
+				Castbar:SetPoint(LolzenUIcfg.nameplates["np_cb_anchor"], health, LolzenUIcfg.nameplates["np_cb_anchor2"], LolzenUIcfg.nameplates["np_cb_posx"], LolzenUIcfg.nameplates["np_cb_posy"])
+				Castbar:SetSize(LolzenUIcfg.nameplates["np_cb_width"], LolzenUIcfg.nameplates["np_cb_height"])			
 				Castbar:SetStatusBarColor(1, 1, 1, 1)
 				frame.Castbar = Castbar
 
 				local Spark = Castbar:CreateTexture(nil, "OVERLAY")
-				Spark:SetSize(6, 4)
+				Spark:SetSize(LolzenUIcfg.nameplates["np_spark_width"], LolzenUIcfg.nameplates["np_spark_height"])
 				Spark:SetBlendMode("ADD")
 				Spark:SetParent(Castbar)
 				frame.Castbar.Spark = Spark
 
 				local icon = Castbar:CreateTexture(nil, "BACKGROUND")
-				icon:SetHeight(8)
-				icon:SetWidth(8)
+				icon:SetSize(LolzenUIcfg.nameplates["np_cbicon_size"], LolzenUIcfg.nameplates["np_cbicon_size"])
 				icon:SetTexCoord(.07, .93, .07, .93)
-				icon:SetPoint("RIGHT", health, "LEFT", -4, 0)
+				icon:SetPoint(LolzenUIcfg.nameplates["np_cbicon_anchor"], health, LolzenUIcfg.nameplates["np_cbicon_anchor2"], LolzenUIcfg.nameplates["np_cbicon_posx"], LolzenUIcfg.nameplates["np_cbicon_posy"])
 				frame.Castbar.Icon = icon
 
 				local Time = Castbar:CreateFontString(nil, "OVERLAY")
-				Time:SetPoint("LEFT", Castbar, "LEFT", 2, -5)
-				Time:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\DroidSansBold.ttf", 6 ,"OUTLINE")
+				Time:SetPoint(LolzenUIcfg.nameplates["np_cbtime_anchor"], Castbar, LolzenUIcfg.nameplates["np_cbtime_anchor2"], LolzenUIcfg.nameplates["np_cbtime_posx"], LolzenUIcfg.nameplates["np_cbtime_posy"])
+				Time:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\"..LolzenUIcfg.nameplates["np_cbtime_font"], LolzenUIcfg.nameplates["np_cbtime_font_size"], LolzenUIcfg.nameplates["np_cbtime_font_flag"])
 				Time:SetTextColor(1, 1, 1)
 				frame.Castbar.Time = Time
 
 				local cbtext = Castbar:CreateFontString(nil, "OVERLAY")
-				cbtext:SetPoint("RIGHT", Castbar, "RIGHT", -2, -5)
-				cbtext:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\DroidSans.ttf", 6 ,"OUTLINE")
+				cbtext:SetPoint(LolzenUIcfg.nameplates["np_cbtext_anchor"], Castbar, LolzenUIcfg.nameplates["np_cbtext_anchor2"], LolzenUIcfg.nameplates["np_cbtext_posx"], LolzenUIcfg.nameplates["np_cbtext_posy"])
+				cbtext:SetFont("Interface\\AddOns\\LolzenUI\\fonts\\"..LolzenUIcfg.nameplates["np_cbtext_font"], LolzenUIcfg.nameplates["np_cbtext_font_size"], LolzenUIcfg.nameplates["np_cbtext_font_flag"])
 				cbtext:SetTextColor(1, 1, 1)
 				frame.Castbar.Text = cbtext
 
 				local Shield = Castbar:CreateTexture(nil, 'OVERLAY')
 				Shield:SetSize(20, 20)
-				Shield:SetTexture("Interface\\CastingBar\\UI-CastingBar-Arena-Shield")
-				Shield:SetPoint("CENTER", icon, 2, 0)
+				Shield:SetTexture("Interface\\AddOns\\LolzenUI\\media\\shield")
+				Shield:SetPoint("CENTER", icon, 0, 0)
 				frame.Castbar.Shield = Shield
 
-				local RaidTargetIndicator = health:CreateTexture(nil, 'OVERLAY')
-				RaidTargetIndicator:SetSize(16, 16)
-				RaidTargetIndicator:SetPoint('TOPRIGHT', health, -2, 14)
-				frame.RaidTargetIndicator = RaidTargetIndicator
+				if LolzenUIcfg.nameplates["np_raidtargetindicator"] == true then
+					local RaidTargetIndicator = health:CreateTexture(nil, 'OVERLAY')
+					RaidTargetIndicator:SetSize(LolzenUIcfg.nameplates["np_raidmark_size"], LolzenUIcfg.nameplates["np_raidmark_size"])
+					RaidTargetIndicator:SetPoint(LolzenUIcfg.nameplates["np_raidmark_anchor"], health, LolzenUIcfg.nameplates["np_raidmark_posx"], LolzenUIcfg.nameplates["np_raidmark_posy"])
+					frame.RaidTargetIndicator = RaidTargetIndicator
+				end
 
-				local Glow = CreateFrame("Frame", nil, frame)
-				Glow:SetBackdrop({
-					edgeFile ="Interface\\AddOns\\LolzenUI\\media\\glow", edgeSize = 5,
-					insets = {left = 4, right = 4, top = 4, bottom = 4}
-				})
-				Glow:SetPoint("TOPLEFT", health, -5, 5)
-				Glow:SetPoint("BOTTOMRIGHT", health, 5, -5)
-				Glow:SetBackdropBorderColor(6, 0, 0)
-				
-				local targetindicator = health:CreateTexture(nil, 'OVERLAY')
-				targetindicator:SetPoint("LEFT", health, 20, -3)
-				targetindicator:SetTexture("Interface\\AddOns\\LolzenUI\\media\\target-glow")
-				targetindicator:SetSize(100*cvars.nameplateSelectedScale, 5*cvars.nameplateSelectedScale)
-				targetindicator:SetVertexColor(48/255, 113/255, 191/255)
-				frame.Targetindicator = targetindicator
-				frame:RegisterEvent("PLAYER_TARGET_CHANGED", UpdateTargetIndicator)
+				if LolzenUIcfg.nameplates["np_targetindicator"] == true then
+					local targetindicator = health:CreateTexture(nil, 'OVERLAY')
+					targetindicator:SetPoint("LEFT", health, 0, -3)
+					targetindicator:SetTexture("Interface\\AddOns\\LolzenUI\\media\\target-glow")
+					targetindicator:SetSize(LolzenUIcfg.nameplates["np_width"], 5*cvars.nameplateSelectedScale)
+					targetindicator:SetVertexColor(48/255, 113/255, 191/255)
+					frame.Targetindicator = targetindicator
+					frame:RegisterEvent("PLAYER_TARGET_CHANGED", UpdateTargetIndicator)
+				end
 
-				-- workaround so we can actually have an glow border
-				local threat = Glow:CreateTexture(nil, "OVERLAY")
-				frame.ThreatIndicator = threat
+				-- workaround so we can actually have a glow border
+				if LolzenUIcfg.nameplates["np_threatindicator"] == true then
+					local Glow = CreateFrame("Frame", nil, frame)
+					Glow:SetBackdrop({
+						edgeFile ="Interface\\AddOns\\LolzenUI\\media\\glow", edgeSize = 5,
+						insets = {left = 4, right = 4, top = 4, bottom = 4}
+					})
+					Glow:SetPoint("TOPLEFT", health, -5, 5)
+					Glow:SetPoint("BOTTOMRIGHT", health, 5, -5)
+					Glow:SetBackdropBorderColor(6, 0, 0)
+
+					local threat = Glow:CreateTexture(nil, "OVERLAY")
+					frame.ThreatIndicator = threat
+					threat.PostUpdate = PostUpdateThreat
+				end
 
 				-- set size and points
-				frame:SetSize(100, 4)
+				frame:SetSize(LolzenUIcfg.nameplates["np_width"], LolzenUIcfg.nameplates["np_height"])
 				frame:SetPoint("CENTER", 0, 0)
 
 				Castbar.PostChannelStart = PostCastStart
 				Castbar.PostCastStart = PostCastStart
-
-				threat.PostUpdate = PostUpdateThreat
 			end
 		end)
 		oUF:SpawnNamePlates(nil, UpdateTargetIndicator, cvars)
