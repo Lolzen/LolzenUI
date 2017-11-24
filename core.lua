0ff6371f455656914cde4bac9c3cc9734edce384
Georg Eitler <funer@gmx.at>
2017-11-24 17:40:54 +0100

    core: make things more logically readable

--- core.lua
+++ core.lua
@@ -1,11 +1,11 @@
 ﻿--// core //--
 
 local addon, ns = ...
+-- make the ns available to the seperate options addon
+_G[addon] = ns
 
 -- Create an empty table which will be filled by modules
 ns.modules = {}
--- make the ns available to the seperate options addon
-_G[addon] = ns
 
 ns.RegisterModule = function(module)
 	if not ns.modules[module] then
