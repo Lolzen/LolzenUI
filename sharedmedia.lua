bf6e9793b0fbdc331f0372c99fedc8d018ad20f8
Georg Eitler <funer@gmx.at>
2017-11-28 17:08:18 +0100

    sharedmedia: initialize
    register LolzenUI's default fonts, border & statusbar with
    LibSharedMedia

---
+++ sharedmedia.lua
@@ -0,0 +1,15 @@
+﻿--// sharedmedia //--
+-- standard textures, fonts, sounds, etc from LolzenUI
+
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- fonts
+LSM:Register("font", "DroidSans", [[Interface\Addons\LolzenUI\fonts\DroidSans.ttf]])
+LSM:Register("font", "DroidSansBold", [[Interface\Addons\LolzenUI\fonts\DroidSansBold.ttf]])
+LSM:Register("font", "Semplice Regular", [[Interface\Addons\LolzenUI\fonts\SEMPRG.ttf]])
+
+--borders
+LSM:Register("border", "LolzenUI Standard", [[Interface\Addons\LolzenUI\media\border]])
+
+--statusbars
+LSM:Register("statusbar", "LolzenUI standard", [[Interface\Addons\LolzenUI\media\statusbar]])
