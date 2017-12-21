e8b3634153b939c135ae638ab1eef998d9fd1d64
Georg Eitler <funer@gmx.at>
2017-12-21 12:44:33 +0100

    buffs: fix taint
    http://www.wowinterface.com/forums/showthread.php?t=55928

--- modules/buffs/buffs.lua
+++ modules/buffs/buffs.lua
@@ -11,74 +11,66 @@ f:SetScript("OnEvent", function(self, event, addon)
 	if addon == "LolzenUI" then
 		if LolzenUIcfg.modules["buffs"] == false then return end
 
-		-- Change the position
-		BuffFrame:ClearAllPoints()
-		BuffFrame:SetPoint(LolzenUIcfg.buffs["buff_anchor1"], LolzenUIcfg.buffs["buff_parent"], LolzenUIcfg.buffs["buff_anchor2"], LolzenUIcfg.buffs["buff_posx"], LolzenUIcfg.buffs["buff_posy"])
-		BuffFrame.SetPoint = function() end
-		
 		local function StyleBuffs(buttonName, index)
 			local button = _G[buttonName..index]
-			if button and not button.modded then
+			if not button or button.modded then return end
 
-				-- Size
-				if buttonName == "BuffButton" then
-					button:SetSize(LolzenUIcfg.buffs["buff_size"], LolzenUIcfg.buffs["buff_size"])
-				elseif buttonName == "DebuffButton" then
-					button:SetSize(LolzenUIcfg.buffs["buff_debuff_size"], LolzenUIcfg.buffs["buff_debuff_size"])
-				elseif buttonName == "TempEnchant" then
-					button:SetSize(LolzenUIcfg.buffs["buff_tempenchant_size"], LolzenUIcfg.buffs["buff_tempenchant_size"])
-				end
-
-				-- Border
-				local border = _G[buttonName..index.."Border"]
-				if border then
-					-- Debuffborder
-					border:SetParent(button)
-					border:SetTexture("Interface\\AddOns\\LolzenUI\\media\\"..LolzenUIcfg.buffs["buff_debuff_texture"])
-					border:SetAllPoints(button)
-					border:SetTexCoord(0, 1, 0, 1)
-				else
-					-- Auraborder
-					if not button.border then
-						button.border = button:CreateTexture(nil, "BORDER")
-						button.border:SetTexture("Interface\\AddOns\\LolzenUI\\media\\"..LolzenUIcfg.buffs["buff_aura_texture"])
-						button.border:SetAllPoints(button)
-						button.border:SetVertexColor(0, 0, 0)
-					end
-				end
-
-				-- Change the look of the Icon slightly
-				local icon = _G[buttonName..index.."Icon"]
-				if icon then
-					icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-					icon:SetPoint("TOPLEFT", button ,"TOPLEFT", 2, -2)
-					icon:SetPoint("BOTTOMRIGHT", button ,"BOTTOMRIGHT", -2, 2)
-				end
-
-				-- Reposition BuffTimeduration
-				button.duration:ClearAllPoints()
-				button.duration:SetPoint(LolzenUIcfg.buffs["buff_duration_anchor1"], button, LolzenUIcfg.buffs["buff_duration_anchor2"], LolzenUIcfg.buffs["buff_duration_posx"], LolzenUIcfg.buffs["buff_duration_posy"])
-				button.duration:SetFont(LSM:Fetch("font", LolzenUIcfg.buffs["buff_duration_font"]), LolzenUIcfg.buffs["buff_duration_font_size"], LolzenUIcfg.buffs["buff_duration_font_flag"])
-				button.duration:SetDrawLayer("OVERLAY")
-
-				-- Reposition buffcounters
-				button.count:ClearAllPoints()
-				button.count:SetPoint(LolzenUIcfg.buffs["buff_counter_anchor"], button, LolzenUIcfg.buffs["buff_counter_posx"], LolzenUIcfg.buffs["buff_counter_posy"])
-				button.count:SetFont(LSM:Fetch("font", LolzenUIcfg.buffs["buff_counter_font"]), LolzenUIcfg.buffs["buff_counter_size"], LolzenUIcfg.buffs["buff_counter_font_flag"])
-				button.count:SetDrawLayer("OVERLAY")
-
-				button.modded = true
+			-- Size
+			if buttonName == "BuffButton" then
+				button:SetSize(LolzenUIcfg.buffs["buff_size"], LolzenUIcfg.buffs["buff_size"])
+			elseif buttonName == "DebuffButton" then
+				button:SetSize(LolzenUIcfg.buffs["buff_debuff_size"], LolzenUIcfg.buffs["buff_debuff_size"])
+			elseif buttonName == "TempEnchant" then
+				button:SetSize(LolzenUIcfg.buffs["buff_tempenchant_size"], LolzenUIcfg.buffs["buff_tempenchant_size"])
 			end
+
+			-- Border
+			local border = _G[buttonName..index.."Border"]
+			if border then
+				-- Debuffborder
+				border:SetParent(button)
+				border:SetTexture("Interface\\AddOns\\LolzenUI\\media\\"..LolzenUIcfg.buffs["buff_debuff_texture"])
+				border:SetAllPoints(button)
+				border:SetTexCoord(0, 1, 0, 1)
+			else
+				-- Auraborder
+				if not button.border then
+					button.border = button:CreateTexture(nil, "BORDER")
+					button.border:SetTexture("Interface\\AddOns\\LolzenUI\\media\\"..LolzenUIcfg.buffs["buff_aura_texture"])
+					button.border:SetAllPoints(button)
+					button.border:SetVertexColor(0, 0, 0)
+				end
+			end
+
+			-- Change the look of the Icon slightly
+			local icon = _G[buttonName..index.."Icon"]
+			if icon then
+				icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+				icon:SetPoint("TOPLEFT", button ,"TOPLEFT", 2, -2)
+				icon:SetPoint("BOTTOMRIGHT", button ,"BOTTOMRIGHT", -2, 2)
+			end
+
+			-- Reposition BuffTimeduration
+			button.duration:ClearAllPoints()
+			button.duration:SetPoint(LolzenUIcfg.buffs["buff_duration_anchor1"], button, LolzenUIcfg.buffs["buff_duration_anchor2"], LolzenUIcfg.buffs["buff_duration_posx"], LolzenUIcfg.buffs["buff_duration_posy"])
+			button.duration:SetFont(LSM:Fetch("font", LolzenUIcfg.buffs["buff_duration_font"]), LolzenUIcfg.buffs["buff_duration_font_size"], LolzenUIcfg.buffs["buff_duration_font_flag"])
+			button.duration:SetDrawLayer("OVERLAY")
+
+			-- Reposition buffcounters
+			button.count:ClearAllPoints()
+			button.count:SetPoint(LolzenUIcfg.buffs["buff_counter_anchor"], button, LolzenUIcfg.buffs["buff_counter_posx"], LolzenUIcfg.buffs["buff_counter_posy"])
+			button.count:SetFont(LSM:Fetch("font", LolzenUIcfg.buffs["buff_counter_font"]), LolzenUIcfg.buffs["buff_counter_size"], LolzenUIcfg.buffs["buff_counter_font_flag"])
+			button.count:SetDrawLayer("OVERLAY")
+
+			button.modded = true
 		end
 
 		local function UpdateAura()
+			-- Change the position
+			BuffFrame:ClearAllPoints()
+			BuffFrame:SetPoint(LolzenUIcfg.buffs["buff_anchor1"], LolzenUIcfg.buffs["buff_parent"], LolzenUIcfg.buffs["buff_anchor2"], LolzenUIcfg.buffs["buff_posx"], LolzenUIcfg.buffs["buff_posy"])
 			for i = 1, BUFF_ACTUAL_DISPLAY do
 				StyleBuffs("BuffButton", i)
-			--		if i == 1 then
-			--			_G["BuffButton"..i]:SetPoint(LolzenUIcfg.buffs["buff_anchor1"], LolzenUIcfg.buffs["buff_parent"], LolzenUIcfg.buffs["buff_anchor2"], LolzenUIcfg.buffs["buff_posx"], LolzenUIcfg.buffs["buff_posy"])
-				--	else
-				--		_G["BuffButton"..i]:SetPoint("RIGHT", _G["BuffButton"..i-1], "LEFT", LolzenUIcfg.buffs["buff_spacing_horizontal"], 0)
-				--	end
 			end
 			for i = 1, DEBUFF_MAX_DISPLAY do
 				StyleBuffs("DebuffButton", i)
