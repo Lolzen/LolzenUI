d7e6dddc3985fc5bcf086d9a29fb41f3fac2a7ba
Georg Eitler <funer@gmx.at>
2018-01-17 16:43:03 +0100

    versioncheck: fix typo

--- modules/versioncheck/versioncheck.lua
+++ modules/versioncheck/versioncheck.lua
@@ -14,7 +14,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 		local addonversion = GetAddOnMetadata(addon, "Version")
 		if not string.find(addonversion, version) then
 			print("|cff5599ffLolzenUI|r: Version numbers don't match!")
-			print("Please look for an update at: http://www.wowinterface.com/downloads/info24512-LolzenUI.html or https://github.com/Lolzen/LolzenUI (dev verion)")
+			print("Please look for an update at: http://www.wowinterface.com/downloads/info24512-LolzenUI.html or https://github.com/Lolzen/LolzenUI (dev version)")
 			print("WoW patch version: "..version..", LolzenUI version: "..addonversion)
 		end
 	end
