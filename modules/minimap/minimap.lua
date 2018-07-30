af59acbe0eb740b9090499beacfe34a9920a1cba
Georg Eitler <funer@gmx.at>
2018-07-30 19:42:25 +0200

    minimap: adapt to realtime options

--- modules/minimap/minimap.lua
+++ modules/minimap/minimap.lua
@@ -14,26 +14,48 @@ local frames = {
 	GameTimeFrame,
 }
 
-local function modifyMinimap()
-	Minimap:SetPoint("TOPRIGHT", OrderHallClassBar, -15, -40)
-
+local function UpdateMiniMapMask()
 	if LolzenUIcfg.minimap["minimap_square"] == true then
-		local overlay = Minimap:CreateTexture(nil,"OVERLAY")
-		overlay:SetTexture("Interface\\AddOns\\LolzenUI\\media\\overlay")
-		overlay:SetPoint("TOPLEFT", Minimap, "TOPLEFT", -1, 1)
-		overlay:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT", 1, -1)
-		overlay:SetWidth(177)
-		overlay:SetHeight(177)
-		overlay:SetVertexColor(0, 0, 0)
+		if not Minimap.overlay then
+			Minimap.overlay = Minimap:CreateTexture(nil,"OVERLAY")
+			Minimap.overlay:SetVertexColor(0, 0, 0)
+			QueueStatusMinimapButton:SetFrameStrata("HIGH")
+		end
+		Minimap.overlay:SetTexture("Interface\\AddOns\\LolzenUI\\media\\overlay")
+		Minimap.overlay:SetPoint("TOPLEFT", Minimap, "TOPLEFT", -1, 1)
+		Minimap.overlay:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT", 1, -1)
+		Minimap.overlay:SetWidth(177)
+		Minimap.overlay:SetHeight(177)
+		
+		MiniMapTracking:ClearAllPoints()
+		MiniMapTracking:SetPoint("BOTTOMRIGHT", -5, 0)
+		MiniMapMailFrame:ClearAllPoints()
+		MiniMapMailFrame:SetPoint("TOPLEFT", 5, -5)
+		CalFrame:ClearAllPoints()
+		CalFrame:SetPoint("BOTTOMLEFT", Minimap, 15, 5)
 
 		Minimap:SetMaskTexture("Interface\\AddOns\\LolzenUI\\media\\Mask")
 	else
 		Minimap:SetMaskTexture("Textures\\MinimapMask")
-		local circle = Minimap:CreateTexture(nil,"OVERLAY")
-		circle:SetAllPoints(Minimap)
-		circle:SetTexture("Interface\\Minimap\\UI-QuestBlob-MinimapRing")
-		circle:SetVertexColor(0, 0, 0)
+		if not Minimap.overlay then
+			Minimap.overlay = Minimap:CreateTexture(nil,"OVERLAY")
+			Minimap.overlay:SetVertexColor(0, 0, 0)
+			QueueStatusMinimapButton:SetFrameStrata("HIGH")
+		end
+		Minimap.overlay:SetAllPoints(Minimap)
+		Minimap.overlay:SetTexture("Interface\\Minimap\\UI-QuestBlob-MinimapRing")
+		
+		MiniMapTracking:ClearAllPoints()
+		MiniMapTracking:SetPoint("BOTTOMRIGHT", -5, 0)
+		MiniMapMailFrame:ClearAllPoints()
+		MiniMapMailFrame:SetPoint("TOP", 0, -5)
+		CalFrame:ClearAllPoints()
+		CalFrame:SetPoint("BOTTOM", Minimap, 0, 5)
 	end
+end
+
+local function modifyMinimap()
+	Minimap:SetPoint("TOPRIGHT", OrderHallClassBar, -15, -40)
 
 	Minimap:EnableMouseWheel(true)
 	Minimap:SetScript("OnMouseWheel", function(self, z)
@@ -50,39 +72,22 @@ local function modifyMinimap()
 	MiniMapTracking:SetParent(Minimap)
 	MiniMapTracking:ClearAllPoints()
 	MiniMapTrackingIcon:SetTexCoord(0.065,0.935,0.065,0.935)
-	if LolzenUIcfg.minimap["minimap_square"] == true then
-		MiniMapTracking:SetPoint("BOTTOMRIGHT", -5, 0)
-	else
-		MiniMapTracking:SetPoint("BOTTOMRIGHT", -5, 0)
-	end
 
 	MiniMapMailBorder:Hide()
 	MiniMapMailFrame:SetParent(Minimap)
 	MiniMapMailFrame:ClearAllPoints()
-	if LolzenUIcfg.minimap["minimap_square"] == true then
-		MiniMapMailFrame:SetPoint("TOPLEFT", 5, -5)
-	else
-		MiniMapMailFrame:SetPoint("TOP", 0, -5)
-	end
 	MiniMapMailIcon:SetTexture("Interface/Minimap/Tracking/Mailbox")
 
 	QueueStatusMinimapButtonBorder:Hide()
 	QueueStatusMinimapButton:SetParent(Minimap)
 	QueueStatusMinimapButton:ClearAllPoints()
 	QueueStatusMinimapButton:SetPoint("TOPRIGHT", -5, -5)
-	if LolzenUIcfg.minimap["minimap_square"] == false then
-		QueueStatusMinimapButton:SetFrameStrata("HIGH")
-	end
+	
 
 	-- let's make our own CalendarFrame =]
 	local date = C_Calendar.GetDate()
 
 	local CalFrame = CreateFrame("Button", "CalFrame", Minimap)
-	if LolzenUIcfg.minimap["minimap_square"] == true then
-		CalFrame:SetPoint("BOTTOMLEFT", Minimap, 15, 5)
-	else
-		CalFrame:SetPoint("BOTTOM", Minimap, 0, 5)
-	end
 	CalFrame:SetHeight(20)
 	CalFrame:SetWidth(14)
 
@@ -113,6 +118,8 @@ local function modifyMinimap()
 	GarrisonLandingPageMinimapButton:HookScript("OnShow", function() GarrisonLandingPageMinimapButton:Hide() end)
 	GarrisonLandingPageMinimapButton.IsShown = function() return true end
 
+	UpdateMiniMapMask()
+
 	for _, frame in pairs(frames) do
 		frame:Hide()
 	end
@@ -124,5 +131,10 @@ f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function(self, event, addon)
 	if LolzenUIcfg.modules["minimap"] == false then Minimap:SetMaskTexture("Textures\\MinimapMask"); return end
 
+	ns.UpdateVariables_minimap = function(self)
+		UpdateMiniMapMask()
+	end
+
 	modifyMinimap()
 end)
+
