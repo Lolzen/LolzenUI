346f9674464ea91e7a9cec8ba2c554b39c1c1de9
Georg <funer@gmx.at>
2019-09-11 09:03:33 +0200

    itemleve: add Inventorian support
    with that most known Bag Addons (which don't provide support for itemlevels on their own) are now supported
    fixes #12

---
+++ modules/itemlevel/ilvl_inventorian.lua
@@ -0,0 +1,58 @@
+--// itemlevel (Inventorian) // --
+
+local _, ns = ...
+local LSM = LibStub("LibSharedMedia-3.0")
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, addon)
+	if addon == "LolzenUI" then
+		if LolzenUIcfg.modules["itemlevel"] == false then return end
+		if not IsAddOnLoaded("Inventorian") then return end
+
+		local function getItemlvlBags(bagID, slotID)
+			local itemLink = GetContainerItemLink(bagID, slotID)
+			local item = Item:CreateFromBagAndSlot(bagID, slotID)
+			if item and IsEquippableItem(itemLink) then
+				return item:GetCurrentItemLevel()
+			end
+		end
+
+		local function getColorItemQualityBags(bagID, slotID)
+			local itemLink = GetContainerItemLink(bagID, slotID)
+			if itemLink then
+				local quality = select(3, GetItemInfo(itemLink))
+				if quality then
+					local r, g, b = GetItemQualityColor(quality)
+					return r, g, b
+				end
+			end
+		end
+
+		-- Bags & Bank (Inventorian)
+		local function updateBagSlotInfo(button)
+			local bag = button:GetParent():GetID()
+			if not button.str then
+				button.str = button:CreateFontString(nil, "OVERLAY")
+				button.str:SetFont(LSM:Fetch("font", LolzenUIcfg.itemlevel["ilvl_font"]), LolzenUIcfg.itemlevel["ilvl_font_size"], LolzenUIcfg.itemlevel["ilvl_font_flag"])
+				button.str:SetPoint(LolzenUIcfg.itemlevel["ilvl_anchor"], button, LolzenUIcfg.itemlevel["ilvl_font_posx"], LolzenUIcfg.itemlevel["ilvl_font_posy"])
+				button.str:SetText(getItemlvlBags(bag, button:GetID()))
+				if LolzenUIcfg.itemlevel["ilvl_use_itemquality_color"] == true then
+					button.str:SetTextColor(getColorItemQualityBags(bag, button:GetID()))
+				else
+					button.str:SetTextColor(unpack(LolzenUIcfg.itemlevel["ilvl_font_color"]))
+				end
+			else
+				button.str:SetText(getItemlvlBags(bag, button:GetID()))
+				if LolzenUIcfg.itemlevel["ilvl_use_itemquality_color"] == true then
+					button.str:SetTextColor(getColorItemQualityBags(bag, button:GetID()))
+				end
+			end
+		end
+
+		if LolzenUIcfg.itemlevel["ilvl_bags"] == true then
+			local Inventorian = LibStub and LibStub("AceAddon-3.0", true) and LibStub("AceAddon-3.0"):GetAddon("Inventorian", true)
+			hooksecurefunc(Inventorian.Item.prototype, "Update", updateBagSlotInfo)
+		end
+	end
+end)
