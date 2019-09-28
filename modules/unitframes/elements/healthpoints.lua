a250f00c2b7b7b6b8926d970f4277a3e870719b6
Georg <funer@gmx.at>
2019-09-28 20:38:42 +0200

    unitframes: elements: healthpoints: remove color codes from tags

--- modules/unitframes/elements/healthpoints.lua
+++ modules/unitframes/elements/healthpoints.lua
@@ -7,14 +7,14 @@ function ns.AddHealthPoints(self, unit)
 	HealthPoints:SetPoint(LolzenUIcfg.unitframes.general["uf_general_hp_anchor"], LolzenUIcfg.unitframes.general["uf_general_hp_posx"], LolzenUIcfg.unitframes.general["uf_general_hp_posy"])
 	if LolzenUIcfg.unitframes.general["uf_use_val_and_perc"] == true then
 		if LolzenUIcfg.unitframes.general["uf_perc_first"] == true then
-			self:Tag(HealthPoints, "[|cffc41f3b>dead<|r][|cff999999>offline<|r][perhp]%"..LolzenUIcfg.unitframes.general["uf_val_perc_divider"].."[lolzen:health]")
+			self:Tag(HealthPoints, "[dead][offline][perhp]%"..LolzenUIcfg.unitframes.general["uf_val_perc_divider"].."[lolzen:health]")
 		else
-			self:Tag(HealthPoints, "[|cffc41f3b>dead<|r][|cff999999>offline<|r][lolzen:health]"..LolzenUIcfg.unitframes.general["uf_val_perc_divider"].."[perhp]%")
+			self:Tag(HealthPoints, "[dead][offline][lolzen:health]"..LolzenUIcfg.unitframes.general["uf_val_perc_divider"].."[perhp]%")
 		end
 	elseif LolzenUIcfg.unitframes.general["uf_use_hp_percent"] == true and LolzenUIcfg.unitframes.general["uf_use_val_and_perc"] == false then
-		self:Tag(HealthPoints, "[|cffc41f3b>dead<|r][|cff999999>offline<|r][perhp]%")
+		self:Tag(HealthPoints, "[dead][offline][perhp]%")
 	else
-		self:Tag(HealthPoints, "[|cffc41f3b>dead<|r][|cff999999>offline<|r][lolzen:health]")
+		self:Tag(HealthPoints, "[dead][offline][lolzen:health]")
 	end
 
 	self.Health.value = HealthPoints
