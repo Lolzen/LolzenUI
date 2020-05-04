d3204126211ffdd1d0015a191c287230660b0bc6
Georg Eitler <funer@gmx.at>
2020-05-04 15:59:21 +0200

    minimap: add realtime option functions
    also fix round minimap mask

--- modules/minimap/minimap.lua
+++ modules/minimap/minimap.lua
@@ -15,28 +15,49 @@ local frames = {
 	GameTimeFrame,
 }
 
-local function modifyMinimap()
-	Minimap:ClearAllPoints()
-	Minimap:SetPoint(LolzenUIcfg.minimap["minimap_anchor"], OrderHallClassBar, LolzenUIcfg.minimap["minimap_posx"], LolzenUIcfg.minimap["minimap_posy"])
-
+local function setMiniMapShape()
 	if LolzenUIcfg.minimap["minimap_square"] == true then
-		local overlay = Minimap:CreateTexture(nil,"OVERLAY")
-		overlay:SetTexture("Interface\\AddOns\\LolzenUI\\media\\overlay")
-		overlay:SetPoint("TOPLEFT", Minimap, "TOPLEFT", -1, 1)
-		overlay:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT", 1, -1)
-		overlay:SetWidth(177)
-		overlay:SetHeight(177)
-		overlay:SetVertexColor(0, 0, 0)
+		if not Minimap.overlay then
+			Minimap.overlay = Minimap:CreateTexture(nil, "OVERLAY")
+			Minimap.overlay:SetVertexColor(0, 0, 0)
+		end
+		Minimap.overlay:SetTexture("Interface\\AddOns\\LolzenUI\\media\\overlay")
+		Minimap.overlay:ClearAllPoints()
+		Minimap.overlay:SetPoint("TOPLEFT", Minimap, "TOPLEFT", -1, 1)
+		Minimap.overlay:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT", 1, -1)
+		Minimap.overlay:SetWidth(177)
+		Minimap.overlay:SetHeight(177)
 
 		Minimap:SetMaskTexture("Interface\\AddOns\\LolzenUI\\media\\Mask")
-	else
-		Minimap:SetMaskTexture("Textures\\MinimapMask")
-		local circle = Minimap:CreateTexture(nil,"OVERLAY")
-		circle:SetAllPoints(Minimap)
-		circle:SetTexture("Interface\\Minimap\\UI-QuestBlob-MinimapRing")
-		circle:SetVertexColor(0, 0, 0)
-	end
 
+		MiniMapMailFrame:ClearAllPoints()
+		MiniMapMailFrame:SetPoint("TOPLEFT", 5, -5)
+		Minimap.CalFrame:ClearAllPoints()
+		Minimap.CalFrame:SetPoint("BOTTOMLEFT", Minimap, 15, 5)
+	else
+		if not Minimap.overlay then
+			Minimap.overlay = Minimap:CreateTexture(nil, "OVERLAY")
+			Minimap.overlay:SetVertexColor(0, 0, 0)
+		end
+		Minimap.overlay:SetTexture("Interface\\Minimap\\UI-QuestBlob-MinimapRing")
+		Minimap.overlay:ClearAllPoints()
+		Minimap.overlay:SetAllPoints(Minimap)
+
+		Minimap:SetMaskTexture(186178) -- Credits to Simpy, see https://git.tukui.org/elvui/elvui/-/commit/b0190888fc105225df7e29a46c12f19825977827
+
+		MiniMapMailFrame:ClearAllPoints()
+		MiniMapMailFrame:SetPoint("TOP", 0, -5)
+		Minimap.CalFrame:ClearAllPoints()
+		Minimap.CalFrame:SetPoint("BOTTOM", Minimap, 0, 5)
+	end
+end
+
+local function setMiniMapPosition()
+	Minimap:ClearAllPoints()
+	Minimap:SetPoint(LolzenUIcfg.minimap["minimap_anchor"], OrderHallClassBar, LolzenUIcfg.minimap["minimap_posx"], LolzenUIcfg.minimap["minimap_posy"])
+end
+
+local function modifyMinimap()
 	Minimap:EnableMouseWheel(true)
 	Minimap:SetScript("OnMouseWheel", function(self, z)
 		local c = Minimap:GetZoom()
@@ -52,29 +73,20 @@ local function modifyMinimap()
 	MiniMapTracking:SetParent(Minimap)
 	MiniMapTracking:ClearAllPoints()
 	MiniMapTrackingIcon:SetTexCoord(0.065,0.935,0.065,0.935)
-	if LolzenUIcfg.minimap["minimap_square"] == true then
-		MiniMapTracking:SetPoint("BOTTOMRIGHT", -5, 0)
-	else
-		MiniMapTracking:SetPoint("BOTTOMRIGHT", -5, 0)
-	end
+	MiniMapTracking:SetPoint("BOTTOMRIGHT", -5, 0)
 
 	MiniMapMailBorder:Hide()
 	MiniMapMailFrame:SetParent(Minimap)
 	MiniMapMailFrame:ClearAllPoints()
-	if LolzenUIcfg.minimap["minimap_square"] == true then
-		MiniMapMailFrame:SetPoint("TOPLEFT", 5, -5)
-	else
-		MiniMapMailFrame:SetPoint("TOP", 0, -5)
-	end
 	MiniMapMailIcon:SetTexture("Interface/Minimap/Tracking/Mailbox")
 
 	QueueStatusMinimapButtonBorder:Hide()
 	QueueStatusMinimapButton:SetParent(Minimap)
 	QueueStatusMinimapButton:ClearAllPoints()
 	QueueStatusMinimapButton:SetPoint("TOPRIGHT", -5, -5)
-	if LolzenUIcfg.minimap["minimap_square"] == false then
+--	if LolzenUIcfg.minimap["minimap_square"] == false then
 		QueueStatusMinimapButton:SetFrameStrata("HIGH")
-	end
+--	end
 
 	-- let's make our own CalendarFrame =]
 	-- C_DateAndTime.GetCurrentCalendarTime() is unreliable on login and returns nil untin a /reload
@@ -85,24 +97,19 @@ local function modifyMinimap()
 		day = day:gsub("0","")
 	end
 
-	local CalFrame = CreateFrame("Button", "CalFrame", Minimap)
-	if LolzenUIcfg.minimap["minimap_square"] == true then
-		CalFrame:SetPoint("BOTTOMLEFT", Minimap, 15, 5)
-	else
-		CalFrame:SetPoint("BOTTOM", Minimap, 0, 5)
-	end
-	CalFrame:SetHeight(20)
-	CalFrame:SetWidth(14)
+	Minimap.CalFrame = CreateFrame("Button", "CalFrame", Minimap)
+	Minimap.CalFrame:SetHeight(20)
+	Minimap.CalFrame:SetWidth(14)
 
-	local CalFont =  CalFrame:CreateFontString(nil, "OVERLAY")
-	CalFont:SetPoint("CENTER", CalFrame, "CENTER")
+	local CalFont =  Minimap.CalFrame:CreateFontString(nil, "OVERLAY")
+	CalFont:SetPoint("CENTER", Minimap.CalFrame, "CENTER")
 	CalFont:SetFont("Fonts\\FRIZQT__.TTF", 20, "OUTLINE")
 	CalFont:SetTextColor(1,1,1)
 	--CalFont:SetText(date.monthDay)
 	CalFont:SetText(day)
 
 	--Now we want to click the numbers to open the calendarframe, don't we?
-	CalFrame:SetScript("OnClick", function()
+	Minimap.CalFrame:SetScript("OnClick", function()
 		if not CalendarFrame then
 			LoadAddOn("Blizzard_Calendar")
 			ShowUIPanel(CalendarFrame)
@@ -133,5 +140,15 @@ f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function(self, event, addon)
 	if LolzenUIcfg.modules["minimap"] == false then Minimap:SetMaskTexture("Textures\\MinimapMask"); return end
 
+	setMiniMapPosition()
 	modifyMinimap()
+	setMiniMapShape()
+
+	ns.setMiniMapShape = function()
+		setMiniMapShape()
+	end
+
+	ns.setMiniMapPosition = function()
+		setMiniMapPosition()
+	end
 end)
