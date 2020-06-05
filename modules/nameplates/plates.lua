1c172c043e155f072af0c8ce5947a39e98c1e255
Georg Eitler <funer@gmx.at>
2020-06-05 09:12:58 +0200

    nameplates: add functions  for realtime options [1/2]

--- modules/nameplates/plates.lua
+++ modules/nameplates/plates.lua
@@ -32,7 +32,7 @@ f:SetScript("OnEvent", function(self, event, addon)
 				ClassNameplateManaBarFrame:Hide()
 			end
 			if not frame then return end
-			if UnitIsUnit(frame.unit, "target") then
+			if LolzenUIcfg.nameplates.general["np_targetindicator"] == true and UnitIsUnit(frame.unit, "target") then
 				frame.Targetindicator:SetAlpha(1)
 			else
 				frame.Targetindicator:SetAlpha(0)
@@ -40,34 +40,38 @@ f:SetScript("OnEvent", function(self, event, addon)
 		end
 
 		local UpdateThreat = function(frame, event, unit)
-			local status = UnitThreatSituation("player", unit)
-			-- do a different behaviour on Tank specc
-			local _, _, _, _, role = GetSpecializationInfoByID(GetInspectSpecialization("player"))
-			if UnitGroupRolesAssigned("player") == "TANK" or role == "TANK" then
-				if status and status > 0 then
-					frame.Glow:Show()
-					-- tanking securely
-					if status == 3 then
-						frame.Glow:SetBackdropBorderColor(0, 6, 0, 1)
-					--insecurely tanking
-					elseif status == 2 then
-						frame.Glow:SetBackdropBorderColor(6, 6, 0, 1)
-					--not tanking
-					elseif status == 1 then
-						frame.Glow:SetBackdropBorderColor(6, 0, 0, 1)
-					-- infight, not tanking
-					elseif status == 0 then
-						frame.Glow:SetBackdropBorderColor(1, 1, 1, 1)
+			if LolzenUIcfg.nameplates.general["np_threatindicator"] == true then
+				local status = UnitThreatSituation("player", unit)
+				-- do a different behaviour on Tank specc
+				local _, _, _, _, role = GetSpecializationInfoByID(GetInspectSpecialization("player"))
+				if UnitGroupRolesAssigned("player") == "TANK" or role == "TANK" then
+					if status and status > 0 then
+						frame.Glow:Show()
+						-- tanking securely
+						if status == 3 then
+							frame.Glow:SetBackdropBorderColor(0, 6, 0, 1)
+						--insecurely tanking
+						elseif status == 2 then
+							frame.Glow:SetBackdropBorderColor(6, 6, 0, 1)
+						--not tanking
+						elseif status == 1 then
+							frame.Glow:SetBackdropBorderColor(6, 0, 0, 1)
+						-- infight, not tanking
+						elseif status == 0 then
+							frame.Glow:SetBackdropBorderColor(1, 1, 1, 1)
+						end
+					else
+						frame.Glow:SetBackdropBorderColor(0, 0, 0, 0)
 					end
 				else
-					frame.Glow:SetBackdropBorderColor(0, 0, 0, 0)
+					if status and status > 0 then
+						frame.Glow:SetBackdropBorderColor(1, 0, 0, 1)
+					else
+						frame.Glow:SetBackdropBorderColor(0, 0, 0, 0)
+					end
 				end
 			else
-				if status and status > 0 then
-					frame.Glow:SetBackdropBorderColor(1, 0, 0, 1)
-				else
-					frame.Glow:SetBackdropBorderColor(0, 0, 0, 0)
-				end
+				frame.Glow:SetBackdropBorderColor(0, 0, 0, 0)
 			end
 		end
 
@@ -86,6 +90,14 @@ f:SetScript("OnEvent", function(self, event, addon)
 				frame.Power:Show()
 			else
 				frame.Power:Hide()
+			end
+		end
+
+		local UpdateRaidMarks = function(frame)
+			if LolzenUIcfg.nameplates.general["np_raidtargetindicator"] == true then
+				frame.RaidTargetIndicator:SetAlpha(1)
+			else
+				frame.RaidTargetIndicator:SetAlpha(0)
 			end
 		end
 
@@ -122,13 +134,14 @@ f:SetScript("OnEvent", function(self, event, addon)
 		end
 
 		local PostUpdateIcon = function(icons, unit, button, index, offset, filter, isDebuff)
-			if LolzenUIcfg.nameplates.general["np_aura_desature_nonplayer_auras"] == true then
-				local texture = button.icon
+			if LolzenUIcfg.nameplates.general["np_aura_desature_nonplayer_auras"] == true then 
 				if button.isPlayer then
-					texture:SetDesaturated(false)
+					button.icon:SetDesaturated(false)
 				else
-					texture:SetDesaturated(true)
+					button.icon:SetDesaturated(true)
 				end
+			else
+				button.icon:SetDesaturated(false)
 			end
 		end
 
@@ -223,39 +236,35 @@ f:SetScript("OnEvent", function(self, event, addon)
 				Shield:SetPoint("CENTER", icon, 0, 0)
 				frame.Castbar.Shield = Shield
 
-				if LolzenUIcfg.nameplates.general["np_raidtargetindicator"] == true then
-					local RaidTargetIndicator = health:CreateTexture(nil, 'OVERLAY')
-					RaidTargetIndicator:SetSize(LolzenUIcfg.nameplates.general["np_raidmark_size"], LolzenUIcfg.nameplates.general["np_raidmark_size"])
-					RaidTargetIndicator:SetPoint(LolzenUIcfg.nameplates.general["np_raidmark_anchor"], health, LolzenUIcfg.nameplates.general["np_raidmark_posx"], LolzenUIcfg.nameplates.general["np_raidmark_posy"])
-					frame.RaidTargetIndicator = RaidTargetIndicator
-				end
+				local RaidTargetIndicator = health:CreateTexture(nil, 'OVERLAY')
+				RaidTargetIndicator:SetSize(LolzenUIcfg.nameplates.general["np_raidmark_size"], LolzenUIcfg.nameplates.general["np_raidmark_size"])
+				RaidTargetIndicator:SetPoint(LolzenUIcfg.nameplates.general["np_raidmark_anchor"], health, LolzenUIcfg.nameplates.general["np_raidmark_posx"], LolzenUIcfg.nameplates.general["np_raidmark_posy"])
+				frame.RaidTargetIndicator = RaidTargetIndicator
+				table.insert(frame.__elements, UpdateRaidMarks)
+				frame:RegisterEvent("PLAYER_TARGET_CHANGED", UpdateRaidMarks, true)
 
-				if LolzenUIcfg.nameplates.general["np_targetindicator"] == true then
-					local targetindicator = health:CreateTexture(nil, 'OVERLAY')
-					targetindicator:SetPoint("LEFT", health, 0, -3)
-					targetindicator:SetTexture("Interface\\AddOns\\LolzenUI\\media\\target-glow")
-					targetindicator:SetSize(LolzenUIcfg.nameplates.general["np_width"], 5*cvars.nameplateSelectedScale)
-					targetindicator:SetVertexColor(48/255, 113/255, 191/255)
-					frame.Targetindicator = targetindicator
-					frame:RegisterEvent("PLAYER_TARGET_CHANGED", UpdateTargetIndicator, true)
-				end
+				local targetindicator = health:CreateTexture(nil, 'OVERLAY')
+				targetindicator:SetPoint("LEFT", health, 0, -3)
+				targetindicator:SetTexture("Interface\\AddOns\\LolzenUI\\media\\target-glow")
+				targetindicator:SetSize(LolzenUIcfg.nameplates.general["np_width"], 5*cvars.nameplateSelectedScale)
+				targetindicator:SetVertexColor(48/255, 113/255, 191/255)
+				frame.Targetindicator = targetindicator
+				frame:RegisterEvent("PLAYER_TARGET_CHANGED", UpdateTargetIndicator, true)
 
-				if LolzenUIcfg.nameplates.general["np_threatindicator"] == true then
-					local Glow = CreateFrame("Frame", nil, frame)
-					Glow:SetBackdrop({
-						edgeFile ="Interface\\AddOns\\LolzenUI\\media\\glow", edgeSize = 5,
-						insets = {left = 4, right = 4, top = 4, bottom = 4}
-					})
-					Glow:SetPoint("TOPLEFT", health, -5, 5)
-					Glow:SetPoint("BOTTOMRIGHT", health, 5, -5)
-					Glow:SetBackdropBorderColor(6, 0, 0)
-					frame.Glow = Glow
+				local Glow = CreateFrame("Frame", nil, frame)
+				Glow:SetBackdrop({
+					edgeFile ="Interface\\AddOns\\LolzenUI\\media\\glow", edgeSize = 5,
+					insets = {left = 4, right = 4, top = 4, bottom = 4}
+				})
+				Glow:SetPoint("TOPLEFT", health, -5, 5)
+				Glow:SetPoint("BOTTOMRIGHT", health, 5, -5)
+				Glow:SetBackdropBorderColor(6, 0, 0)
+				frame.Glow = Glow
 
-					frame.ThreatIndicator = {
-						IsObjectType = function() end,
-						Override = UpdateThreat,
-					}
-				end
+				frame.ThreatIndicator = {
+					IsObjectType = function() end,
+					Override = UpdateThreat,
+				}
 
 				local exploGlow = CreateFrame("Frame", nil, frame)
 				exploGlow:SetBackdrop({
@@ -318,5 +327,196 @@ f:SetScript("OnEvent", function(self, event, addon)
 			end
 		end)
 		oUF:SpawnNamePlates("Lolzen - Nameplates", UpdateTargetIndicator, cvars)
+
+		ns.setNPTargetIndicator = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					UpdateTargetIndicator(v)
+				end
+			end
+		end
+
+		ns.setNPSize = function()
+			SetCVar("nameplateSelectedScale", LolzenUIcfg.nameplates.general["np_selected_scale"])
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v:SetSize(LolzenUIcfg.nameplates.general["np_width"], LolzenUIcfg.nameplates.general["np_height"])
+				end
+			end
+		end
+
+		ns.setNPTexture = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					UpdateExplosiveAndPRD(v, v.unit)
+				end
+			end
+		end
+
+		ns.setNPLevelFont = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v.Level:SetFont(LSM:Fetch("font", LolzenUIcfg.nameplates.general["np_lvlname_font"]), LolzenUIcfg.nameplates.general["np_lvlname_font_size"], LolzenUIcfg.nameplates.general["np_lvlname_font_flag"])
+				end
+			end
+		end
+
+		ns.setNPLevelFontPos = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v.Level:ClearAllPoints()
+					v.Level:SetPoint(LolzenUIcfg.nameplates.general["np_lvlname_anchor"], v.Health, LolzenUIcfg.nameplates.general["np_lvlname_posx"], LolzenUIcfg.nameplates.general["np_lvlname_posy"]) 
+				end
+			end
+		end
+
+		ns.setNPRaidMarks = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					UpdateRaidMarks(v)
+				end
+			end
+		end
+
+		ns.setNPRaidMarkSize = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v.RaidTargetIndicator:SetSize(LolzenUIcfg.nameplates.general["np_raidmark_size"], LolzenUIcfg.nameplates.general["np_raidmark_size"])
+				end
+			end
+		end
+
+		ns.setNPRaidMarkPos = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v.RaidTargetIndicator:ClearAllPoints()
+					v.RaidTargetIndicator:SetPoint(LolzenUIcfg.nameplates.general["np_raidmark_anchor"], v.Health, LolzenUIcfg.nameplates.general["np_raidmark_posx"], LolzenUIcfg.nameplates.general["np_raidmark_posy"])
+				end
+			end
+		end
+		
+		ns.setNPAuraType = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					if LolzenUIcfg.nameplates.general["np_aura_show_type"] == "Buffs" then
+						if v.Debuffs then
+							v.Debuffs:Hide()
+						elseif v.Auras then
+							v.Auras:Hide()
+						end
+						v.Buffs:Show()
+					elseif LolzenUIcfg.nameplates.general["np_aura_show_type"] == "Debuffs" then
+						if v.Buffs then
+							v.Buffs:Hide()
+						elseif v.Auras then
+							v.Auras:Hide()
+						end
+						v.Debuffs:Show()
+					elseif LolzenUIcfg.nameplates.general["np_aura_show_type"] == "Both" then
+						if v.Buffs then
+							v.Buffs:Hide()
+						elseif v.Debuffs then
+							v.Debuffs:Hide()
+						end
+						v.Auras:Show()
+					else
+						if v.Buffs then
+							v.Buffs:Hide()
+						elseif v.Debuffs then
+							v.Debuffs:Hide()
+						elseif v.Auras then
+							v.Auras:Hide()
+						end
+					end
+				end
+			end
+		end
+
+		ns.setNPAuraMaxNum = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v.Buffs.num = LolzenUIcfg.nameplates.general["np_aura_maxnum"]
+					v.Debuffs.num = LolzenUIcfg.nameplates.general["np_aura_maxnum"]
+					v.Auras.numTotal = LolzenUIcfg.nameplates.general["np_aura_maxnum"]
+				end
+			end
+		end
+
+		ns.setNPAuraSpacing = function()
+			--todo really refresh "old" auras
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v.Buffs.spacing = LolzenUIcfg.nameplates.general["np_aura_spacing"]
+					v.Debuffs.spacing = LolzenUIcfg.nameplates.general["np_aura_spacing"]
+					v.Auras.spacing = LolzenUIcfg.nameplates.general["np_aura_spacing"]
+				end
+			end
+		end
+
+		ns.setNPAuraSize = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v.Buffs.size = LolzenUIcfg.nameplates.general["np_aura_size"]
+					v.Debuffs.size = LolzenUIcfg.nameplates.general["np_aura_size"]
+					v.Auras.size = LolzenUIcfg.nameplates.general["np_aura_size"]
+				end
+			end
+		end
+
+		ns.setNPAuraPos = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					if LolzenUIcfg.nameplates.general["np_aura_show_type"] == "Buffs" then
+						v.Buffs:ClearAllPoints()
+						v.Buffs:SetPoint(LolzenUIcfg.nameplates.general["np_aura_anchor1"], v, LolzenUIcfg.nameplates.general["np_aura_anchor2"], LolzenUIcfg.nameplates.general["np_aura_posx"], LolzenUIcfg.nameplates.general["np_aura_posy"])
+					elseif LolzenUIcfg.nameplates.general["np_aura_show_type"] == "Debuffs" then
+						v.Debuffs:ClearAllPoints()
+						v.Debuffs:SetPoint(LolzenUIcfg.nameplates.general["np_aura_anchor1"], v, LolzenUIcfg.nameplates.general["np_aura_anchor2"], LolzenUIcfg.nameplates.general["np_aura_posx"], LolzenUIcfg.nameplates.general["np_aura_posy"])
+					elseif LolzenUIcfg.nameplates.general["np_aura_show_type"] == "Both" then
+						v.Auras:ClearAllPoints()
+						v.Auras:SetPoint(LolzenUIcfg.nameplates.general["np_aura_anchor1"], v, LolzenUIcfg.nameplates.general["np_aura_anchor2"], LolzenUIcfg.nameplates.general["np_aura_posx"], LolzenUIcfg.nameplates.general["np_aura_posy"])
+					end
+				end
+			end
+		end
+
+		ns.setNPAuraAxis = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					v.Buffs["growth-x"] = LolzenUIcfg.nameplates.general["np_aura_growth_x"]
+					v.Buffs["growth-y"] = LolzenUIcfg.nameplates.general["np_aura_growth_y"]
+					v.Debuffs["growth-x"] = LolzenUIcfg.nameplates.general["np_aura_growth_x"]
+					v.Debuffs["growth-y"] = LolzenUIcfg.nameplates.general["np_aura_growth_y"]
+					v.Auras["growth-x"] = LolzenUIcfg.nameplates.general["np_aura_growth_x"]
+					v.Auras["growth-y"] = LolzenUIcfg.nameplates.general["np_aura_growth_y"]
+				end
+			end
+		end
+
+		ns.setNPAuraShowOnlyPlayer = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					if LolzenUIcfg.nameplates.general["np_aura_show_only_player"] == true then
+						v.Buffs.onlyShowPlayer = true
+						v.Debuffs.onlyShowPlayer = true
+						v.Auras.onlyShowPlayer = true
+					else
+						v.Buffs.onlyShowPlayer = false
+						v.Debuffs.onlyShowPlayer = false
+						v.Auras.onlyShowPlayer = false
+					end
+				end
+			end
+		end
+
+		ns.setNPAuraDesatured = function()
+			for i, v in pairs(oUF.objects) do
+				if v.unit:match("nameplate") then
+					PostUpdateIcon(v.Buffs)
+					PostUpdateIcon(v.Debuffs)
+					PostUpdateIcon(v.Auras)
+				end
+			end
+		end
 	end
 end)
