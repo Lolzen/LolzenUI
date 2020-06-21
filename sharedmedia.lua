b9397dbee540ff0bab0602e3f6316f8e43cab9c2
Georg Eitler <funer@gmx.at>
2020-06-21 18:13:05 +0200

    sharedmedia/sounds: add UT3_female sounds

--- sharedmedia.lua
+++ sharedmedia.lua
@@ -32,6 +32,19 @@ LSM:Register("sound", "UT2004_female: eight", [[Interface\Addons\LolzenUI\sounds
 LSM:Register("sound", "UT2004_female: nine", [[Interface\Addons\LolzenUI\sounds\UT2004_female\nine.mp3]])
 LSM:Register("sound", "UT2004_female: ten", [[Interface\Addons\LolzenUI\sounds\UT2004_female\ten.mp3]])
 
+LSM:Register("sound", "UT3_female: Denied", [[Interface\Addons\LolzenUI\sounds\UT3_female\Denied.ogg]])
+LSM:Register("sound", "UT3_female: Play", [[Interface\Addons\LolzenUI\sounds\UT3_female\Play.ogg]])
+LSM:Register("sound", "UT3_female: one", [[Interface\Addons\LolzenUI\sounds\UT3_female\one.ogg]])
+LSM:Register("sound", "UT3_female: two", [[Interface\Addons\LolzenUI\sounds\UT3_female\two.ogg]])
+LSM:Register("sound", "UT3_female: three", [[Interface\Addons\LolzenUI\sounds\UT3_female\three.ogg]])
+LSM:Register("sound", "UT3_female: four", [[Interface\Addons\LolzenUI\sounds\UT3_female\four.ogg]])
+LSM:Register("sound", "UT3_female: five", [[Interface\Addons\LolzenUI\sounds\UT3_female\five.ogg]])
+LSM:Register("sound", "UT3_female: six", [[Interface\Addons\LolzenUI\sounds\UT3_female\six.ogg]])
+LSM:Register("sound", "UT3_female: seven", [[Interface\Addons\LolzenUI\sounds\UT3_female\seven.ogg]])
+LSM:Register("sound", "UT3_female: eight", [[Interface\Addons\LolzenUI\sounds\UT3_female\eight.ogg]])
+LSM:Register("sound", "UT3_female: nine", [[Interface\Addons\LolzenUI\sounds\UT3_female\nine.ogg]])
+LSM:Register("sound", "UT3_female: ten", [[Interface\Addons\LolzenUI\sounds\UT3_female\ten.ogg]])
+
 -- [buttons]
 -- border
 LBT:Register("border", "LolzenUI Standard", [[Interface\Addons\LolzenUI\media\gloss]])
