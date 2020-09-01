e657197d0a71ecc2ce28e1c9ae2bd2a58a0b6be1
Georg Eitler <funer@gmx.at>
2020-09-01 16:10:59 +0200

    unitframes: targettarget: functions for realtime options

--- modules/unitframes/units/targettarget.lua
+++ modules/unitframes/units/targettarget.lua
@@ -16,4 +16,33 @@ ns.SetupTargetTarget = function(self, unit)
 
 	ns.AddCombatFade(self, unit)
 	ns.AddRaidMark(self, unit)
+
+	ns.SetUFToTSize = function()
+		self:SetSize(LolzenUIcfg.unitframes.targettarget["uf_targettarget_width"], LolzenUIcfg.unitframes.targettarget["uf_targettarget_height"])
+	end
+
+	ns.SetUFToTOwnFont = function()
+		if LolzenUIcfg.unitframes.targettarget["uf_targettarget_use_own_hp_font_settings"] == true then
+			self.Health.value:SetFont(LSM:Fetch("font", LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_font"]), LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_font_size"], LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_font_flag"])
+			self.Health.value:ClearAllPoints()
+			self.Health.value:SetPoint(LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_anchor"], LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_posx"], LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_posy"])
+		else
+			self.Health.value:SetFont(LSM:Fetch("font", LolzenUIcfg.unitframes.general["uf_general_hp_font"]), LolzenUIcfg.unitframes.general["uf_general_hp_font_size"], LolzenUIcfg.unitframes.general["uf_general_hp_font_flag"])
+			self.Health.value:ClearAllPoints()
+			self.Health.value:SetPoint(LolzenUIcfg.unitframes.general["uf_general_hp_anchor"], LolzenUIcfg.unitframes.general["uf_general_hp_posx"], LolzenUIcfg.unitframes.general["uf_general_hp_posy"])
+		end
+	end
+
+	ns.SetUFToTHPFont = function()
+		if LolzenUIcfg.unitframes.targettarget["uf_targettarget_use_own_hp_font_settings"] == true then
+			self.Health.value:SetFont(LSM:Fetch("font", LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_font"]), LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_font_size"], LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_font_flag"])
+		end
+	end
+
+	ns.SetUFToTHPPos = function()
+		if LolzenUIcfg.unitframes.targettarget["uf_targettarget_use_own_hp_font_settings"] == true then
+			self.Health.value:ClearAllPoints()
+			self.Health.value:SetPoint(LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_anchor"], LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_posx"], LolzenUIcfg.unitframes.targettarget["uf_targettarget_hp_posy"])
+		end
+	end
 end
