ae502e7add85abc40f3493869187ca1e2adb7d63
Georg Eitler <funer@gmx.at>
2020-10-22 12:08:29 +0200

    unitframes: units: shared: functions for realtimeoptions regarding powercolors

--- modules/unitframes/units/shared.lua
+++ modules/unitframes/units/shared.lua
@@ -1,5 +1,6 @@
 local _, ns = ...
 local LSM = LibStub("LibSharedMedia-3.0")
+local oUF = ns.oUF
 
 ns.shared = function(self, unit)
 	self.colors.power["MANA"] = LolzenUIcfg.unitframes.powercolors[0]
@@ -32,4 +33,159 @@ ns.shared = function(self, unit)
 	ns.AddBorder(self, unit)
 	ns.AddHealthBar(self, unit)
 	ns.AddHealthPoints(self, unit)
+
+	ns.SetUFPowerColorMana = function()
+		self.colors.power["MANA"] = LolzenUIcfg.unitframes.powercolors[0]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorRage = function()
+		self.colors.power["RAGE"] = LolzenUIcfg.unitframes.powercolors[1]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorFocus = function()
+		self.colors.power["FOCUS"] = LolzenUIcfg.unitframes.powercolors[2]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorEnergy = function()
+		self.colors.power["ENERGY"] = LolzenUIcfg.unitframes.powercolors[3]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorCP = function()
+		self.colors.power["COMBO_POINTS"] = LolzenUIcfg.unitframes.powercolors[4]
+		for i, v in pairs(oUF.objects) do
+			if v.unit:match("player") then
+				v.ClassPower:ForceUpdate()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorRunes = function()
+		self.colors.power["RUNES"] = LolzenUIcfg.unitframes.powercolors[5]
+		for i, v in pairs(oUF.objects) do
+			if v.unit:match("player") then
+				v.Runes:ForceUpdate()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorRunePower = function()
+		self.colors.power["RUNIC_POWER"] = LolzenUIcfg.unitframes.powercolors[6]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorSoulshards = function()
+		self.colors.power["SOUL_SHARDS"] = LolzenUIcfg.unitframes.powercolors[7]
+		for i, v in pairs(oUF.objects) do
+			if v.unit:match("player") then
+				v.ClassPower:ForceUpdate()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorLunarPower = function()
+		self.colors.power["LUNAR_POWER"] = LolzenUIcfg.unitframes.powercolors[8]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorHolyPower = function()
+		self.colors.power["HOLY_POWER"] = LolzenUIcfg.unitframes.powercolors[9]
+		for i, v in pairs(oUF.objects) do
+			if v.unit:match("player") then
+				v.ClassPower:ForceUpdate()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorMaelstrom = function()
+		self.colors.power["MAELSTROM"] = LolzenUIcfg.unitframes.powercolors[11]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorChi = function()
+		self.colors.power["CHI"] = LolzenUIcfg.unitframes.powercolors[12]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorInsanity = function()
+		self.colors.power["INSANITY"] = LolzenUIcfg.unitframes.powercolors[13]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorArcaneCharges = function()
+		self.colors.power["ARCANE_CHARGES"] = LolzenUIcfg.unitframes.powercolors[16]
+		for i, v in pairs(oUF.objects) do
+			if v.unit:match("player") then
+				v.ClassPower:ForceUpdate()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorFury = function()
+		self.colors.power["FURY"] = LolzenUIcfg.unitframes.powercolors[17]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
+
+	ns.SetUFPowerColorPain = function()
+		self.colors.power["PAIN"] = LolzenUIcfg.unitframes.powercolors[18]
+		for i, v in pairs(oUF.objects) do
+			if v.Power then
+				v.Power:ForceUpdate()
+				v:UpdateTags()
+			end
+		end
+	end
 end
