b84e7d694db1d09abdf5ab72e094db8ca847d322
Georg Eitler <funer@gmx.at>
2020-10-27 08:57:31 +0100

    unitframes: elements: combatfade: add ability to toggle on/off for realtime options

--- modules/unitframes/elements/combatfade.lua
+++ modules/unitframes/elements/combatfade.lua
@@ -1,6 +1,7 @@
 local _, ns = ...
 
 local CombatFade = function(self, event)
+	if LolzenUIcfg.unitframes.general["uf_fade_combat"] == false then return end
 	local unit = self.unit
 	-- the elements which are to be faded
 	local elements = {
@@ -23,11 +24,35 @@ local CombatFade = function(self, event)
 end
 
 function ns.AddCombatFade(self, unit)
+	table.insert(self.__elements, CombatFade)
 	if LolzenUIcfg.unitframes.general["uf_fade_combat"] == true then
-		table.insert(self.__elements, CombatFade)
 		self:RegisterEvent("PLAYER_REGEN_ENABLED", CombatFade, true)
 		self:RegisterEvent('PLAYER_REGEN_DISABLED', CombatFade, true)
 		self:RegisterEvent("PLAYER_TARGET_CHANGED", CombatFade, true)
 		self:RegisterEvent("UNIT_TARGET", CombatFade)
+	end
+end
+
+function ns.EnableCombatFade(self, unit)
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", CombatFade, true)
+	self:RegisterEvent('PLAYER_REGEN_DISABLED', CombatFade, true)
+	self:RegisterEvent("PLAYER_TARGET_CHANGED", CombatFade, true)
+	self:RegisterEvent("UNIT_TARGET", CombatFade)
+end
+
+function ns.DisableCombatFade(self, unit)
+	self:UnregisterEvent("PLAYER_REGEN_ENABLED", CombatFade, true)
+	self:UnregisterEvent('PLAYER_REGEN_DISABLED', CombatFade, true)
+	self:UnregisterEvent("PLAYER_TARGET_CHANGED", CombatFade, true)
+	self:UnregisterEvent("UNIT_TARGET", CombatFade)
+	local elements = {
+		self.Health,
+		self.Power,
+		self.Border,
+		self.Panel,
+		self.Background,
+	}
+	for _, element in pairs(elements) do
+		UIFrameFadeIn(element, 0.3, element:GetAlpha(), 1)
 	end
 end
